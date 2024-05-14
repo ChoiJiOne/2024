d76d65a45529d1cefb4dcb00272d3e3392c78acd
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-05-14 22:25:49 +0900

    3D 기하 도형을 렌더링하는 테스트 코드 추가

--- Picking3D/Src/Main.cpp
+++ Picking3D/Src/Main.cpp
@@ -3,7 +3,10 @@
 
 #include "Assertion.h"
 #include "CrashModule.h"
+#include "GameModule.h"
 #include "PlatformModule.h"
+
+#include "GeometryRenderer3D.h"
 #include "RenderModule.h"
 
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
@@ -19,17 +22,31 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 	HWND windowHandle = PlatformModule::GetWindowHandle();
 
 	RenderModule::Init(windowHandle);
+	GameModule::Init();
+
+	GeometryRenderer3D* renderer = RenderModule::CreateResource<GeometryRenderer3D>();
+
+	int32_t screenWidth = 0;
+	int32_t screenHeight = 0;
+	RenderModule::GetScreenSize(screenWidth, screenHeight);
+	float aspect = static_cast<float>(screenWidth) / static_cast<float>(screenHeight);
+
+	renderer->SetView(Mat4x4::LookAt(Vec3f(10.0f, 10.0f, 10.0f), Vec3f(0.0f, 0.0f, 0.0f), Vec3f(0.0f, 1.0f, 0.0f)));
+	renderer->SetProjection(Mat4x4::Perspective(MathModule::ToRadian(45.0f), aspect, 0.01f, 100.0f));
 
 	PlatformModule::SetEndLoopCallback([&]() { RenderModule::Uninit(); });
 
 	PlatformModule::RunLoop(
 		[&](float deltaSeconds)
 		{
-			RenderModule::BeginFrame(1.0f, 0.0f, 0.0f, 1.0f);
+			RenderModule::BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
+
+			renderer->DrawGrid3D(Vec3f(100.0f, 100.0f, 100.0f), 1.0f);
 			RenderModule::EndFrame();
 		}
 	);
 
+	GameModule::Uninit();
 	PlatformModule::Uninit();
 	CrashModule::Uninit();
 
