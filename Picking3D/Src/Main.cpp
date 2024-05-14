f649c5f0846ace04452996cc99b5cac8a1b5539e
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-05-14 22:42:01 +0900

    게임 루프에 카메라 엔티티 추가

--- Picking3D/Src/Main.cpp
+++ Picking3D/Src/Main.cpp
@@ -9,13 +9,15 @@
 #include "GeometryRenderer3D.h"
 #include "RenderModule.h"
 
+#include "Camera.h"
+
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
 #if defined(DEBUG_MODE) || defined(RELEASE_MODE) || defined(DEVELOPMENT_MODE)
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 
-	PlatformModule::WindowConstructParams windowParam{ L"Picking3D", 100, 100, 800, 600, false, false };
+	PlatformModule::WindowConstructParams windowParam{ L"Picking3D", 100, 100, 1000, 800, false, false };
 
 	CrashModule::Init();
 	PlatformModule::Init(windowParam);
@@ -25,23 +27,24 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 	GameModule::Init();
 
 	GeometryRenderer3D* renderer = RenderModule::CreateResource<GeometryRenderer3D>();
-
-	int32_t screenWidth = 0;
-	int32_t screenHeight = 0;
-	RenderModule::GetScreenSize(screenWidth, screenHeight);
-	float aspect = static_cast<float>(screenWidth) / static_cast<float>(screenHeight);
-
-	renderer->SetView(Mat4x4::LookAt(Vec3f(10.0f, 10.0f, 10.0f), Vec3f(0.0f, 0.0f, 0.0f), Vec3f(0.0f, 1.0f, 0.0f)));
-	renderer->SetProjection(Mat4x4::Perspective(MathModule::ToRadian(45.0f), aspect, 0.01f, 100.0f));
+	Camera* camera = GameModule::CreateEntity<Camera>();
+	
+	renderer->SetView(camera->GetView());
+	renderer->SetProjection(camera->GetProjection());
 
 	PlatformModule::SetEndLoopCallback([&]() { RenderModule::Uninit(); });
 
 	PlatformModule::RunLoop(
 		[&](float deltaSeconds)
 		{
+			camera->Tick(deltaSeconds);
+			renderer->SetView(camera->GetView());
+			renderer->SetProjection(camera->GetProjection());
+
 			RenderModule::BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
 
 			renderer->DrawGrid3D(Vec3f(100.0f, 100.0f, 100.0f), 1.0f);
+
 			RenderModule::EndFrame();
 		}
 	);
