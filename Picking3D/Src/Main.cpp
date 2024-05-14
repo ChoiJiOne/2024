123700f426992dc2bb7022a2df1a56fecd912ef2
CHOIJIONE\choij <choijione02@gmail.com>
2024-05-14 16:01:39 +0900

    CRT 메모리 누수를 감지하는 기능 추가

--- Picking3D/Src/Main.cpp
+++ Picking3D/Src/Main.cpp
@@ -1,12 +1,17 @@
 #include <cstdint>
 #include <Windows.h>
 
+#include "Assertion.h"
 #include "CrashModule.h"
 #include "PlatformModule.h"
 #include "RenderModule.h"
 
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
+#if defined(DEBUG_MODE) || defined(RELEASE_MODE) || defined(DEVELOPMENT_MODE)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
 	PlatformModule::WindowConstructParams windowParam{ L"Picking3D", 100, 100, 800, 600, false, false };
 
 	CrashModule::Init();
@@ -27,5 +32,10 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 
 	PlatformModule::Uninit();
 	CrashModule::Uninit();
+
+#if defined(DEBUG_MODE) || defined(RELEASE_MODE) || defined(DEVELOPMENT_MODE)
+	CHECK(_CrtCheckMemory());
+#endif
+
 	return 0;
 }
