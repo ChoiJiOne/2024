ce3d0e86474623e420b55f402d59de89ee4521c9
CHOIJIONE\choij <choijione02@gmail.com>
2024-05-15 16:05:16 +0900

    컨트롤 엔티티 추가 및 필요 없어진 코드 삭제

--- Picking3D/Src/Main.cpp
+++ Picking3D/Src/Main.cpp
@@ -8,10 +8,14 @@
 #include "GameModule.h"
 #include "PlatformModule.h"
 
+#include "Checkboard.h"
+#include "GeometryGenerator.h"
 #include "GeometryRenderer3D.h"
+#include "MeshRenderer.h"
 #include "RenderModule.h"
 
 #include "Camera.h"
+#include "Control.h"
 
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
@@ -26,32 +30,52 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 	HWND windowHandle = PlatformModule::GetWindowHandle();
 
 	RenderModule::Init(windowHandle);
+	RenderModule::SetVsyncMode(false);
 	GameModule::Init();
 
-	GeometryRenderer3D* renderer = RenderModule::CreateResource<GeometryRenderer3D>();
-	Camera* camera = GameModule::CreateEntity<Camera>();
+	Checkboard* checkboard = RenderModule::CreateResource<Checkboard>(Checkboard::ESize::Size_1024x1024, Checkboard::ESize::Size_64x64, Vec4f(1.0f, 1.0f, 1.0f, 1.0f), Vec4f(1.0f, 0.0f, 0.0f, 1.0f));
+	GeometryRenderer3D* geometryRenderer = RenderModule::CreateResource<GeometryRenderer3D>();
+	MeshRenderer* meshRenderer = RenderModule::CreateResource<MeshRenderer>();
 	
-	renderer->SetView(camera->GetView());
-	renderer->SetProjection(camera->GetProjection());
+	std::vector<VertexPositionNormalUv3D> vertices;
+	std::vector<uint32_t> indices;
+	GeometryGenerator::CreateSphere(1.0f, 40, vertices, indices);
+
+	StaticMesh<VertexPositionNormalUv3D>* mesh = RenderModule::CreateResource<StaticMesh<VertexPositionNormalUv3D>>(vertices, indices);
+
+	Camera* camera = GameModule::CreateEntity<Camera>();
+	Control* control = GameModule::CreateEntity<Control>(camera);
+
+	geometryRenderer->SetView(camera->GetView());
+	geometryRenderer->SetProjection(camera->GetProjection());
+	meshRenderer->SetView(camera->GetView());
+	meshRenderer->SetProjection(camera->GetProjection());
 
 	PlatformModule::SetEndLoopCallback([&]() { RenderModule::Uninit(); });
-
+	
 	PlatformModule::RunLoop(
 		[&](float deltaSeconds)
 		{
-			ImGui::Begin("Control", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
-			ImGui::SetWindowPos(ImVec2(10.0f, 10.0f));
-			ImGui::SetWindowSize(ImVec2(400.0f, 200.0f));
-			camera->SetActive(!ImGui::IsWindowFocused());
-			ImGui::End();
-
+			control->Tick(deltaSeconds);
 			camera->Tick(deltaSeconds);
-			renderer->SetView(camera->GetView());
-			renderer->SetProjection(camera->GetProjection());
+
+			geometryRenderer->SetView(camera->GetView());
+			geometryRenderer->SetProjection(camera->GetProjection());
+			meshRenderer->SetView(camera->GetView());
+			meshRenderer->SetProjection(camera->GetProjection());
 
 			RenderModule::BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
 
-			renderer->DrawGrid3D(Vec3f(100.0f, 100.0f, 100.0f), 1.0f);
+			geometryRenderer->DrawGrid3D(Vec3f(100.0f, 100.0f, 100.0f), 1.0f);
+
+			//for (float x = -50.0f; x <= 50.0f; x += 1.0f)
+			//{
+			//	for (float z = -50.0f; z <= 50.0f; z += 1.0f)
+			//	{
+			//		Mat4x4 world = Mat4x4::Translation(x, 0.0f, z) * Mat4x4::Scale(0.5f, 0.5f, 0.5f);
+			//		meshRenderer->DrawMesh(mesh, world, checkboard);
+			//	}
+			//}
 
 			RenderModule::EndFrame();
 		}
