34c81f88c02ecfdb8e4d23cc35f2f035496e9ec9
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-05-16 08:57:50 +0900

    쿼터니언 및 충돌 처리 기능 테스트를 위한 준비

--- Picking3D/Src/Main.cpp
+++ Picking3D/Src/Main.cpp
@@ -1,12 +1,15 @@
 #include <cstdint>
 #include <Windows.h>
 
+#include <glad/glad.h>
 #include <imgui.h>
 
 #include "Assertion.h"
 #include "CrashModule.h"
 #include "GameModule.h"
 #include "PlatformModule.h"
+
+#include "Collision.h"
 
 #include "Checkboard.h"
 #include "GeometryGenerator.h"
@@ -23,26 +26,23 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 
-	PlatformModule::WindowConstructParams windowParam{ L"Picking3D", 100, 100, 1000, 800, false, false };
-
 	CrashModule::Init();
-	PlatformModule::Init(windowParam);
-	HWND windowHandle = PlatformModule::GetWindowHandle();
-
-	RenderModule::Init(windowHandle);
-	RenderModule::SetVsyncMode(false);
+	PlatformModule::Init(PlatformModule::WindowConstructParams{ L"Picking3D", 100, 100, 1000, 800, false, false });
+	RenderModule::Init(PlatformModule::GetWindowHandle());
 	GameModule::Init();
 
-	Checkboard* checkboard = RenderModule::CreateResource<Checkboard>(Checkboard::ESize::Size_1024x1024, Checkboard::ESize::Size_64x64, Vec4f(1.0f, 1.0f, 1.0f, 1.0f), Vec4f(1.0f, 0.0f, 0.0f, 1.0f));
-	GeometryRenderer3D* geometryRenderer = RenderModule::CreateResource<GeometryRenderer3D>();
-	MeshRenderer* meshRenderer = RenderModule::CreateResource<MeshRenderer>();
-	
+	RenderModule::SetVsyncMode(false);
+	RenderModule::SetDepthMode(true);
+
 	std::vector<VertexPositionNormalUv3D> vertices;
 	std::vector<uint32_t> indices;
 	GeometryGenerator::CreateSphere(1.0f, 40, vertices, indices);
 
 	StaticMesh<VertexPositionNormalUv3D>* mesh = RenderModule::CreateResource<StaticMesh<VertexPositionNormalUv3D>>(vertices, indices);
-
+	Checkboard* checkboard = RenderModule::CreateResource<Checkboard>(Checkboard::ESize::Size_1024x1024, Checkboard::ESize::Size_64x64, Vec4f(1.0f, 1.0f, 1.0f, 1.0f), Vec4f(1.0f, 0.0f, 0.0f, 1.0f));
+	GeometryRenderer3D* geometryRenderer = RenderModule::CreateResource<GeometryRenderer3D>();
+	MeshRenderer* meshRenderer = RenderModule::CreateResource<MeshRenderer>();
+	
 	Camera* camera = GameModule::CreateEntity<Camera>();
 	Control* control = GameModule::CreateEntity<Control>(camera);
 
@@ -52,7 +52,7 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 	meshRenderer->SetProjection(camera->GetProjection());
 
 	PlatformModule::SetEndLoopCallback([&]() { RenderModule::Uninit(); });
-	
+
 	PlatformModule::RunLoop(
 		[&](float deltaSeconds)
 		{
@@ -67,15 +67,6 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 			RenderModule::BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
 
 			geometryRenderer->DrawGrid3D(Vec3f(100.0f, 100.0f, 100.0f), 1.0f);
-
-			//for (float x = -50.0f; x <= 50.0f; x += 1.0f)
-			//{
-			//	for (float z = -50.0f; z <= 50.0f; z += 1.0f)
-			//	{
-			//		Mat4x4 world = Mat4x4::Translation(x, 0.0f, z) * Mat4x4::Scale(0.5f, 0.5f, 0.5f);
-			//		meshRenderer->DrawMesh(mesh, world, checkboard);
-			//	}
-			//}
 
 			RenderModule::EndFrame();
 		}
