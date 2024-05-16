d1b84920ba5c42c3bc006769500eec3e9cb90ae1
CHOIJIONE\choij <choijione02@gmail.com>
2024-05-16 12:25:43 +0900

    마우스 위치를 기반으로 설정된 광선과 구의 충돌 처리 테스트 코드 추가
    - 구현 실패.

--- Picking3D/Src/Main.cpp
+++ Picking3D/Src/Main.cpp
@@ -8,6 +8,7 @@
 #include "CrashModule.h"
 #include "GameModule.h"
 #include "PlatformModule.h"
+#include "InputController.h"
 
 #include "Collision.h"
 
@@ -19,6 +20,9 @@
 
 #include "Camera.h"
 #include "Control.h"
+
+#include <DirectXMath.h>
+#include <DirectXCollision.h>
 
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
@@ -52,11 +56,20 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 	meshRenderer->SetProjection(camera->GetProjection());
 
 	PlatformModule::SetEndLoopCallback([&]() { RenderModule::Uninit(); });
+	
+	std::vector<Sphere> spheres;
+	for (float x = -3.0f; x <= 3.0f; x += 3.0f)
+	{
+		for (float z = -3.0f; z <= 3.0f; z += 3.0f)
+		{
+			spheres.push_back(Sphere(Vec3f(x, 0.0f, z), 0.3f));
+		}
+	}
 
 	PlatformModule::RunLoop(
 		[&](float deltaSeconds)
 		{
-			control->Tick(deltaSeconds);
+			control->Tick(deltaSeconds);			
 			camera->Tick(deltaSeconds);
 
 			geometryRenderer->SetView(camera->GetView());
@@ -64,9 +77,72 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 			meshRenderer->SetView(camera->GetView());
 			meshRenderer->SetProjection(camera->GetProjection());
 
+			Ray ray;
+			{
+				int32_t screenWidth = 0;
+				int32_t screenHeight = 0;
+				RenderModule::GetScreenSize(screenWidth, screenHeight);
+				CursorPos pos = InputController::GetCurrCursorPos();
+
+				// Step1. 3d Normalised Device Coordinates
+				float x = (2.0f * static_cast<float>(pos.x)) / static_cast<float>(screenWidth) - 1.0f;
+				float y = 1.0f - (2.0f * static_cast<float>(pos.y)) / static_cast<float>(screenHeight);
+				float z = 1.0f;
+				Vec3f direction(x, y, z);
+
+				// Step2. 4d Homogeneous Clip Coordinates
+				Vec4f rayClip = Vec4f(direction.x, direction.y, -1.0f, 1.0f);
+
+				// Step3. 4d Eye (Camera) Coordinates
+				Mat4x4 projectInv = Mat4x4::Inverse(camera->GetProjection());
+				Vec4f rayEye = Vec4f(
+					rayClip.x * projectInv.e00 + rayClip.y * projectInv.e10 + rayClip.z * projectInv.e20 + rayClip.w * projectInv.e30,
+					rayClip.x * projectInv.e01 + rayClip.y * projectInv.e11 + rayClip.z * projectInv.e21 + rayClip.w * projectInv.e31,
+					rayClip.x * projectInv.e02 + rayClip.y * projectInv.e12 + rayClip.z * projectInv.e22 + rayClip.w * projectInv.e32,
+					rayClip.x * projectInv.e03 + rayClip.y * projectInv.e13 + rayClip.z * projectInv.e23 + rayClip.w * projectInv.e33
+				);
+
+				rayEye = Vec4f(rayEye.x, rayEye.y, -1.0f, 1.0f);
+
+				// Step 4: 4d World Coordinates
+				Mat4x4 viewInv = Mat4x4::Inverse(camera->GetView());
+				Vec4f rayWorld4 = Vec4f(
+					rayEye.x * viewInv.e00 + rayEye.y * viewInv.e10 + rayEye.z * viewInv.e20 + rayEye.w * viewInv.e30,
+					rayEye.x * viewInv.e01 + rayEye.y * viewInv.e11 + rayEye.z * viewInv.e21 + rayEye.w * viewInv.e31,
+					rayEye.x * viewInv.e02 + rayEye.y * viewInv.e12 + rayEye.z * viewInv.e22 + rayEye.w * viewInv.e32,
+					rayEye.x * viewInv.e03 + rayEye.y * viewInv.e13 + rayEye.z * viewInv.e23 + rayEye.w * viewInv.e33
+				);
+
+				Vec3f rayWorld = Vec3f(rayWorld4.x, rayWorld4.y, rayWorld4.z);
+				rayWorld = Vec3f::Normalize(rayWorld);
+				ray.direction = rayWorld;
+
+				ImGui::Begin("Collision", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
+				ImGui::SetWindowPos(ImVec2(600.0f, 10.0f));
+				ImGui::SetWindowSize(ImVec2(400.0f, 200.0f));
+				ImGui::Text("direction : %f, %f, %f", direction.x, direction.y, direction.z);
+				ImGui::Text("ray eye : %f, %f, %f", rayEye.x, rayEye.y, rayEye.z);
+				ImGui::Text("ray world : %f, %f, %f", rayWorld.x, rayWorld.y, rayWorld.z);
+				ImGui::End();
+			}
+
 			RenderModule::BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
 
 			geometryRenderer->DrawGrid3D(Vec3f(100.0f, 100.0f, 100.0f), 1.0f);
+			
+			ray.origin = camera->eyePosition_;
+
+			for (const auto& sphere : spheres)
+			{
+				if (Collision::Raycast(ray, sphere))
+				{
+					geometryRenderer->DrawSphere3D(Mat4x4::Translation(sphere.center), sphere.radius, Vec4f(1.0f, 0.0f, 0.0f, 1.0f));
+				}
+				else
+				{
+					geometryRenderer->DrawSphere3D(Mat4x4::Translation(sphere.center), sphere.radius, Vec4f(0.0f, 0.0f, 1.0f, 1.0f));
+				}
+			}
 
 			RenderModule::EndFrame();
 		}
