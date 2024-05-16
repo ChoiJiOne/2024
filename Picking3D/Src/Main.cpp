03f88db78fe238f026888e4625dfe446324f1287
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-05-16 17:19:28 +0900

    마우스에 대응하는 광선을 생성하는 코드 삭제
    - 카메라 엔티티에 구현된 기능으로 필요 없어진 부분 삭제

--- Picking3D/Src/Main.cpp
+++ Picking3D/Src/Main.cpp
@@ -21,60 +21,6 @@
 #include "Camera.h"
 #include "Control.h"
 
-Vec3f Unproject(const Vec3f& viewportPosition, const Vec2f& viewportOrigin, const Vec2f& viewportSize, const Mat4x4& view, const Mat4x4& projection)
-{
-	float normalized[4] = 
-	{
-		(viewportPosition.x - viewportOrigin.x) / viewportSize.x,
-		(viewportPosition.y - viewportOrigin.y) / viewportSize.y,
-		viewportPosition.z,
-		1.0f
-	};
-
-	float ndcSpace[4] = { normalized[0], normalized[1], normalized[2], normalized[3] };
-
-	ndcSpace[0] = ndcSpace[0] * 2.0f - 1.0f;
-	ndcSpace[1] = 1.0f - ndcSpace[1] * 2.0f;
-	ndcSpace[2] = MathModule::Clamp<float>(ndcSpace[2], 0.0f, 1.0f);
-
-	Mat4x4 projectInv = Mat4x4::Inverse(projection);
-	Vec4f eyeSpace;
-	eyeSpace.x = ndcSpace[0] * projectInv.e00 + ndcSpace[1] * projectInv.e10 + ndcSpace[2] * projectInv.e20 + ndcSpace[3] * projectInv.e30;
-	eyeSpace.y = ndcSpace[0] * projectInv.e01 + ndcSpace[1] * projectInv.e11 + ndcSpace[2] * projectInv.e21 + ndcSpace[3] * projectInv.e31;
-	eyeSpace.z = ndcSpace[0] * projectInv.e02 + ndcSpace[1] * projectInv.e12 + ndcSpace[2] * projectInv.e22 + ndcSpace[3] * projectInv.e32;
-	eyeSpace.w = ndcSpace[0] * projectInv.e03 + ndcSpace[1] * projectInv.e13 + ndcSpace[2] * projectInv.e23 + ndcSpace[3] * projectInv.e33;
-
-	Mat4x4 viewInv = Mat4x4::Inverse(view);
-	Vec4f worldSpace;
-	worldSpace.x = eyeSpace.x * viewInv.e00 + eyeSpace.y * viewInv.e10 + eyeSpace.z * viewInv.e20 + eyeSpace.w * viewInv.e30;
-	worldSpace.y = eyeSpace.x * viewInv.e01 + eyeSpace.y * viewInv.e11 + eyeSpace.z * viewInv.e21 + eyeSpace.w * viewInv.e31;
-	worldSpace.z = eyeSpace.x * viewInv.e02 + eyeSpace.y * viewInv.e12 + eyeSpace.z * viewInv.e22 + eyeSpace.w * viewInv.e32;
-	worldSpace.w = eyeSpace.x * viewInv.e03 + eyeSpace.y * viewInv.e13 + eyeSpace.z * viewInv.e23 + eyeSpace.w * viewInv.e33;
-
-	if (!MathModule::NearZero(worldSpace.w))
-	{
-		worldSpace.x /= worldSpace.w;
-		worldSpace.y /= worldSpace.w;
-		worldSpace.z /= worldSpace.w;
-	}
-
-	return Vec3f(worldSpace.x, worldSpace.y, worldSpace.z);
-}
-
-Ray GetMouseRay(const Vec2f& mousePos, const Vec2f& viewportOrigin, const Vec2f& viewportSize, const Mat4x4& view, const Mat4x4& projection)
-{
-	Vec3f nearPosition(mousePos.x, mousePos.y, 0.0f);
-	Vec3f farPosition(mousePos.x, mousePos.y, 1.0f);
-
-	Vec3f pNear = Unproject(nearPosition, viewportOrigin, viewportSize, view, projection);
-	Vec3f pFar = Unproject(farPosition, viewportOrigin, viewportSize, view, projection);
-
-	Vec3f origin = pNear;
-	Vec3f direction = Vec3f::Normalize(pFar - pNear);
-
-	return Ray(origin, direction);
-}
-
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
 #if defined(DEBUG_MODE) || defined(RELEASE_MODE) || defined(DEVELOPMENT_MODE)
@@ -128,23 +74,13 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 			meshRenderer->SetView(camera->GetView());
 			meshRenderer->SetProjection(camera->GetProjection());
 
-			int32_t width = 0;
-			int32_t height = 0;
-			RenderModule::GetScreenSize(width, height);
-
-			CursorPos pos = InputController::GetCurrCursorPos();
-
-			Vec2f mousePos = Vec2f(static_cast<float>(pos.x), static_cast<float>(pos.y));
-			Vec2f viewportSize = Vec2f(static_cast<float>(width), static_cast<float>(height));
-			Ray ray = GetMouseRay(mousePos, Vec2f(0.0f, 0.0f), viewportSize, camera->GetView(), camera->GetProjection());
-
 			RenderModule::BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
 
 			geometryRenderer->DrawGrid3D(Vec3f(100.0f, 100.0f, 100.0f), 1.0f);
 			
 			for (const auto& aabb : aabbs)
 			{
-				if (Collision::Raycast(ray, aabb))
+				if (Collision::Raycast(camera->GetMouseRay(), aabb))
 				{
 					geometryRenderer->DrawCube3D(Mat4x4::Translation(aabb.center), aabb.extents, Vec4f(1.0f, 0.0f, 0.0f, 1.0f));
 				}
