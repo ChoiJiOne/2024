758fafa12cd3e082c00110ee372fd1397d9887bd
CHOIJIONE\choij <choijione02@gmail.com>
2024-05-22 17:50:06 +0900

    애니메이션 키 프레임을 모아놓은 트랙 탬플릿 클래스 선언 추가

---
+++ Module/AnimationModule/Inc/Track.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <vector>
+
+#include "Interpolation.h"
+#include "Keyframe.h"
+
+
+/**
+ * @brief 애니메이션 키 프레임을 모아놓은 트랙입니다.
+ */
+template <typename T, uint32_t N>
+class Track
+{
+public:
+
+};
