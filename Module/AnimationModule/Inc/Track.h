3e6b0558dc3e423b9b6f326d98666c270eb499df
CHOIJIONE\choij <choijione02@gmail.com>
2024-05-22 18:00:01 +0900

    애니메이션 트랙의 기본적인 생성자 및 가상 소멸자 추가

--- Module/AnimationModule/Inc/Track.h
+++ Module/AnimationModule/Inc/Track.h
@@ -13,5 +13,47 @@ template <typename T, uint32_t N>
 class Track
 {
 public:
+	/**
+	 * @brief 애니메이션 트랙의 기본 생성자입니다.
+	 */
+	Track() = default;
 
+
+	/**
+	 * @brief 애니메이션 트랙의 복사 생성자입니다.
+	 * 
+	 * @param instance 복사할 인스턴스입니다.
+	 */
+	Track(Track&& instance) noexcept
+		: keyframes_(instance.keyframes_)
+		, interpolation_(instance.interpolation_) {}
+
+
+	/**
+	 * @brief 애니메이션 트랙의 복사 생성자입니다.
+	 *
+	 * @param instance 복사할 인스턴스입니다.
+	 */
+	Track(const Track&& instance) noexcept 
+		: keyframes_(instance.keyframes_)
+		, interpolation_(instance.interpolation_) {}
+
+
+	/**
+	 * @brief 애니메이션 트랙의 가상 소멸자입니다.
+	 */
+	virtual ~Track() {}
+	
+
+private:
+	/**
+	 * @brief 트랙 내의 키 프레임 목록입니다.
+	 */
+	std::vector<Keyframe<N>> keyframes_;
+
+
+	/**
+	 * @brief 키 프레임을 보간할 때의 유형입니다.
+	 */
+	EInterpolation interpolation_ = EInterpolation::LINEAR;
 };
