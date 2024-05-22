de1353fd613c372df9d0e8015b3703294dee5927
CHOIJIONE\choij <choijione02@gmail.com>
2024-05-22 18:24:21 +0900

    트랙이 지원하는 타입에 대한 보간 기능 추가
    - 이 기능은 트랙 내부에서만 사용

--- Module/AnimationModule/Src/Track.cpp
+++ Module/AnimationModule/Src/Track.cpp
@@ -6,6 +6,28 @@ template Track<float, 1>;
 template Track<Vec3f, 3>;
 template Track<Quat, 4>;
 
+inline float Interpolation(float a, float b, float t)
+{
+	return a + (b - a) * t;
+}
+
+inline Vec3f Interpolation(const Vec3f& a, const Vec3f& b, float t)
+{
+	return Vec3f::Lerp(a, b, t);
+}
+
+inline Quat Interpolation(const Quat& a, const Quat& b, float t)
+{
+	Quat result = Quat::Lerp(a, b, t);
+
+	if (Quat::Dot(a, b) < 0.0f)
+	{
+		result = Quat::Lerp(a, -b, t);
+	}
+
+	return Quat::Normalize(result);
+}
+
 template<typename T, uint32_t N>
 Keyframe<N>& Track<T, N>::operator[](uint32_t index)
 {
