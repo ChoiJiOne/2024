08d65ad08c0c2568df65bcfc691945ff8fcc0f40
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-05-27 16:26:03 +0900

    애니메이션 클립을 부드럽게 연결하는 크로스 페이드 타겟 클래스 추가

---
+++ Module/AnimationModule/Inc/CrossFadeTarget.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include "Clip.h"
+#include "Pose.h"
+
+
+/**
+ * @brief 애니메이션 패이딩 시 샘플링된 포즈의 상태 값입니다.
+ */
+class CrossFadeTarget
+{
+public:
+	/**
+	 * @brief 크로스 페이드 타겟의 기본 생성자입니다.
+	 */
+	CrossFadeTarget() = default;
+
+
+	/**
+	 * @brief 크로스 페이드 타겟의 생성자입니다.
+	 * 
+	 * @param 크로스 페이드 타겟의 클립입니다.
+	 * @param 크로스 페이드 타겟의 포즈입니다.
+	 * @param 크로스 페이드 타겟의 애니메이션 지속 시간입니다.
+	 */
+	CrossFadeTarget(Clip* target, const Pose& pose, float duration)
+		: clip_(target)
+		, pose_(pose)
+		, time_(target->GetStartTime())
+		, duration_(duration)
+		, elapsed_(0.0f) {}
+
+
+private:
+	/**
+	 * @brief 애니메이션 패이딩 시 샘플링할 포즈입니다.
+	 */
+	Pose pose_;
+
+
+	/**
+	 * @brief 현재의 애니메이션 클립입니다.
+	 */
+	Clip* clip_ = nullptr;
+
+
+	/**
+	 * @brief 누적 시간값입니다.
+	 */
+	float time_ = 0.0f;
+
+
+	/**
+	 * @brief 애니메이션 지속 시간입니다.
+	 */
+	float duration_ = 0.0f;
+
+
+	/**
+	 * @brief 애니메이션 플레이 경과 시간입니다.
+	 */
+	float elapsed_ = 0.0f;
+};
