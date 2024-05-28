80351d496c6cfac8476415cb7cd7222c1602aeef
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-05-28 10:15:43 +0900

    메인 함수에 구현된 부분을 Application class로 분리

--- Animation3D/Src/Main.cpp
+++ Animation3D/Src/Main.cpp
@@ -5,59 +5,7 @@
 #include <crtdbg.h>
 #endif
 
-#include "CrashModule.h"
-#include "GameModule.h"
-#include "PlatformModule.h"
-#include "RenderModule.h"
-#include "GeometryRenderer3D.h"
-#include "GLTFLoader.h"
-#include "Camera.h"
-#include "Pose.h"
-#include "SkinnedMesh.h"
-#include "Checkboard.h"
-#include "Texture2D.h"
-#include "MeshRenderer3D.h"
-#include "CrossFadeController.h"
-#include "Character.h"
-
-void RunApplication()
-{
-	Camera* camera = GameModule::CreateEntity<Camera>();
-	Character* character = GameModule::CreateEntity<Character>();
-
-	GeometryRenderer3D* geometryRenderer = RenderModule::CreateResource<GeometryRenderer3D>();
-	MeshRenderer3D* meshRenderer = RenderModule::CreateResource<MeshRenderer3D>();
-	
-	PlatformModule::RunLoop(
-		[&](float deltaSeconds)
-		{
-			character->Tick(deltaSeconds);
-			camera->Tick(deltaSeconds);
-
-			geometryRenderer->SetView(camera->GetView());
-			geometryRenderer->SetProjection(camera->GetProjection());
-			meshRenderer->SetView(camera->GetView());
-			meshRenderer->SetProjection(camera->GetProjection());
-
-			RenderModule::BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
-
-			geometryRenderer->DrawGrid3D(Vec3f(100.0f, 100.0f, 100.0f), 1.0f);
-
-			std::vector<SkinnedMesh*> meshes = character->GetMeshes();
-
-			for (const auto& mesh : meshes)
-			{
-				mesh->Skin(&character->GetCrossFadeController().GetSkeleton(), &character->GetCrossFadeController().GetCurrentPose());
-				const std::vector<Mat4x4>& bindPose = mesh->GetPosePalette();
-				const std::vector<Mat4x4>& invBindPose = character->GetCrossFadeController().GetSkeleton().GetInvBindPose();
-
-				meshRenderer->DrawSkinnedMesh3D(Mat4x4::Identity(), mesh, bindPose, invBindPose, character->GetMaterial());
-			}
-			
-			RenderModule::EndFrame();
-		}
-	);
-}
+#include "Application.h"
 
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
@@ -65,20 +13,10 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 
-	CrashModule::Init();
-	PlatformModule::Init(PlatformModule::WindowConstructParams{ L"Animation3D", 100, 100, 1000, 800, false, false });
-	RenderModule::Init(PlatformModule::GetWindowHandle());
-	GameModule::Init();
+	std::unique_ptr<Application> app = std::make_unique<Application>();
+	app->Init();
+	app->Run();
+	app.reset();
 
-	PlatformModule::SetEndLoopCallback([&]() { RenderModule::Uninit(); });
-
-	RenderModule::SetAlphaBlendMode(true);
-	RenderModule::SetDepthMode(true);
-
-	RunApplication();
-
-	GameModule::Uninit();
-	PlatformModule::Uninit();
-	CrashModule::Uninit();
 	return 0;
 }
