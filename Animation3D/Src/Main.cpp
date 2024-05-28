d932b503aed89b112fdffe4b7564016a70dc19db
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-05-28 10:00:18 +0900

    게임 루프 부분에 캐릭터 엔티티 추가

--- Animation3D/Src/Main.cpp
+++ Animation3D/Src/Main.cpp
@@ -18,70 +18,20 @@
 #include "Texture2D.h"
 #include "MeshRenderer3D.h"
 #include "CrossFadeController.h"
+#include "Character.h"
 
 void RunApplication()
 {
-	// GLTF 데이터 로딩...
-	cgltf_data* data = GLTFLoader::Load("Resource/Model/Michelle.gltf");
-	Skeleton skeleton = GLTFLoader::LoadSkeleton(data);
-	std::vector<Clip> clips = GLTFLoader::LoadAnimationClips(data);
-	std::vector<GLTFLoader::MeshData> meshData = GLTFLoader::LoadMeshData(data);
-	GLTFLoader::Free(data);
-
-	// 메시 리소스 로딩
-	std::vector<SkinnedMesh*> meshes;
-	for (const auto& mesh : meshData)
-	{
-		uint32_t size = mesh.positions.size();
-
-		std::vector<VertexPositionNormalUvSkin3D> vertices(size);
-		std::vector<uint32_t> indices = mesh.indices;
-
-		for (uint32_t index = 0; index < size; ++index)
-		{
-			vertices[index].position = mesh.positions[index];
-			vertices[index].normal = mesh.normals[index];
-			vertices[index].uv = mesh.texcoords[index];
-			vertices[index].weight = mesh.weights[index];
-			vertices[index].joints = mesh.joints[index];
-		}
-
-		meshes.push_back(RenderModule::CreateResource<SkinnedMesh>(vertices, indices, false));
-	}
-
-	// 매터리얼 로딩
-	Texture2D* material = RenderModule::CreateResource<Texture2D>("Resource/Texture/Michelle.png", false);
-
-	// 카메라 엔티티 생성
 	Camera* camera = GameModule::CreateEntity<Camera>();
+	Character* character = GameModule::CreateEntity<Character>();
 
 	GeometryRenderer3D* geometryRenderer = RenderModule::CreateResource<GeometryRenderer3D>();
 	MeshRenderer3D* meshRenderer = RenderModule::CreateResource<MeshRenderer3D>();
-
-
-	CrossFadeController crossFadeController;
-	crossFadeController.SetSkeleton(skeleton);
-	crossFadeController.Play(clips.data());
-	crossFadeController.Update(0.0f);
-
-	float fadeTime = 3.0f;
-	int32_t currentClip = 0;
-
+	
 	PlatformModule::RunLoop(
 		[&](float deltaSeconds)
 		{
-			crossFadeController.Update(deltaSeconds);
-
-			fadeTime -= deltaSeconds;
-			if (fadeTime < 0.0f)
-			{
-				fadeTime = 3.0f;
-
-				currentClip = (currentClip + 1) % clips.size();
-
-				crossFadeController.FadeTo(&clips[currentClip], 0.5f);
-			}
-
+			character->Tick(deltaSeconds);
 			camera->Tick(deltaSeconds);
 
 			geometryRenderer->SetView(camera->GetView());
@@ -93,13 +43,15 @@ void RunApplication()
 
 			geometryRenderer->DrawGrid3D(Vec3f(100.0f, 100.0f, 100.0f), 1.0f);
 
+			std::vector<SkinnedMesh*> meshes = character->GetMeshes();
+
 			for (const auto& mesh : meshes)
 			{
-				mesh->Skin(&crossFadeController.GetSkeleton(), &crossFadeController.GetCurrentPose());
+				mesh->Skin(&character->GetCrossFadeController().GetSkeleton(), &character->GetCrossFadeController().GetCurrentPose());
 				const std::vector<Mat4x4>& bindPose = mesh->GetPosePalette();
-				const std::vector<Mat4x4>& invBindPose = crossFadeController.GetSkeleton().GetInvBindPose();
+				const std::vector<Mat4x4>& invBindPose = character->GetCrossFadeController().GetSkeleton().GetInvBindPose();
 
-				meshRenderer->DrawSkinnedMesh3D(Mat4x4::Identity(), mesh, bindPose, invBindPose, material);
+				meshRenderer->DrawSkinnedMesh3D(Mat4x4::Identity(), mesh, bindPose, invBindPose, character->GetMaterial());
 			}
 			
 			RenderModule::EndFrame();
