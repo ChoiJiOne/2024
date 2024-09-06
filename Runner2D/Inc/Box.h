8fd9201ed2420c45133f385c40a97ab82e31af6b
choijione <choijione02@gmail.com>
2024-09-06 11:24:17 +0900

    플레이어가 피해야 할 박스 엔티티 추가
    - 플레이어가 박스와 충돌하면 플레이어의 상태를 HURT 상태로 변환하는 기능 추가

---
+++ Runner2D/Inc/Box.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "Collision2D.h"
+
+#include "Entity2D.h"
+
+class Atlas2D;
+class Camera;
+class Player;
+
+class Box : public Entity2D
+{
+public:
+	Box(const Rect2D& bound);
+	virtual ~Box();
+
+	DISALLOW_COPY_AND_ASSIGN(Box);
+
+	virtual void Tick(float deltaSeconds) override;
+	virtual void Render() override;
+	virtual void Release() override;
+
+	Rect2D* GetCollision() { return &bound_; }
+
+	bool CanMove() { return bCanMove_; }
+	void SetMovable(bool bCanMove) { bCanMove_ = bCanMove; }
+
+private:
+	Player* player_ = nullptr;
+	Camera* camera_ = nullptr;
+	Atlas2D* atlas_ = nullptr;
+
+	Rect2D bound_;
+
+	bool bCanMove_ = false;
+	float speed_ = 200.0f;
+};
---
+++ Runner2D/Src/Box.cpp
@@ -0,0 +1,63 @@
+#include "Assertion.h"
+#include "Atlas2D.h"
+#include "EntityManager.h"
+#include "RenderManager2D.h"
+#include "ResourceManager.h"
+
+#include "Box.h"
+#include "Camera.h"
+#include "Player.h"
+
+Box::Box(const Rect2D& bound)
+	: player_(EntityManager::Get().GetByName<Player>("Player"))
+	, camera_(EntityManager::Get().GetByName<Camera>("Camera"))
+	, bound_(bound)
+	, bCanMove_(true)
+{
+	atlas_ = ResourceManager::Get().GetByName<Atlas2D>("Atlas");
+
+	bIsInitialized_ = true;
+}
+
+Box::~Box()
+{
+	if (bIsInitialized_)
+	{
+		Release();
+	}
+}
+
+void Box::Tick(float deltaSeconds)
+{
+	if (!bCanMove_)
+	{
+		return;
+	}
+
+	bound_.center.x -= deltaSeconds * speed_;
+
+	if (!bound_.Intersect(camera_->GetCollision()) && bound_.center.x < 0.0f)
+	{
+		bCanMove_ = false;
+	}
+
+	if (bound_.Intersect(player_->GetCollision()))
+	{
+		player_->SetStatus(Player::EStatus::HURT);
+	}
+}
+
+void Box::Render()
+{
+	RenderManager2D::Get().DrawSprite(atlas_, "Box", bound_.center, bound_.size.x, bound_.size.y);
+}
+
+void Box::Release()
+{
+	CHECK(bIsInitialized_);
+
+	atlas_ = nullptr;
+	camera_ = nullptr;
+
+	bIsInitialized_ = false;
+}
