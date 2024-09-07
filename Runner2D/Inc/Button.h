9625278cb324bca2a45464fae40da54697676976
choijione <choijione02@gmail.com>
2024-09-08 08:58:00 +0900

    버튼을 클릭하고 땠을 때 이벤트를 수행하는 기능 및 화면에 버튼을 렌더링하는 기능 추가

--- Runner2D/Inc/Button.h
+++ Runner2D/Inc/Button.h
@@ -1,26 +1,37 @@
 #pragma once
 
 #include <functional>
+#include <map>
 
 #include "Collision2D.h"
+#include "IApp.h"
 
 #include "Entity2D.h"
 
-class IApp;
 class TTFont;
 
 class Button : public Entity2D
 {
 public:
-	enum class Type
+	struct Layout
 	{
-		LEFT   = (1 << ((1) - 1)),
-		MIDDLE = (1 << ((2) - 1)),
-		RIGHT  = (1 << ((3) - 1)),
+		GameMath::Vec4f textColor;
+		GameMath::Vec4f disableColor;
+		GameMath::Vec4f enableColor;
+		GameMath::Vec4f pressColor;
+		GameMath::Vec4f releaseColor;
+
+		GameMath::Vec2f center;
+		GameMath::Vec2f size;
+		Mouse mouse;
+
+		TTFont* font;
+		std::wstring text;
+		float side;
 	};
 
 public:
-	Button();
+	Button(const Layout& layout, const std::function<void()>& clickEvent);
 	virtual ~Button();
 
 	DISALLOW_COPY_AND_ASSIGN(Button);
@@ -30,5 +41,27 @@ public:
 	virtual void Release() override;
 
 private:
+	enum class State : int32_t
+	{
+		DISABLED = 0x00,
+		ENABLED  = 0x01,
+		PRESSED  = 0x02,
+		RELEASED = 0x03,
+	};
+
+	State GetState(const Press& press, const State& state);
+	bool IsDetectMouseCursor();
+
+private:
 	IApp* app_ = nullptr;
+
+	Layout layout_;
+	Rect2D bound_;
+	State state_ = State::DISABLED;
+
+	GameMath::Vec2f textPos_;
+
+	std::function<void()> clickEvent_ = nullptr;
+
+	std::map<State, GameMath::Vec4f> stateColors_;
 };
--- Runner2D/Src/Button.cpp
+++ Runner2D/Src/Button.cpp
@@ -8,9 +8,25 @@
 
 #include "Button.h"
 
-Button::Button()
+Button::Button(const Layout& layout, const std::function<void()>& clickEvent)
+	: layout_(layout)
+	, clickEvent_(clickEvent)
 {
 	app_ = IApp::Get();
+
+	bound_ = Rect2D(layout_.center, layout_.size);
+
+	GameMath::Vec2f textSize;
+	layout_.font->MeasureText(layout_.text, textSize.x, textSize.y);
+
+	textPos_ = layout_.center + GameMath::Vec2f(-textSize.x * 0.5f, +textSize.y * 0.5f);
+	stateColors_ = 
+	{
+		{ State::DISABLED, layout_.disableColor },
+		{ State::ENABLED,  layout_.enableColor  },
+		{ State::PRESSED,  layout_.pressColor   },
+		{ State::RELEASED, layout_.releaseColor },
+	};
 
 	bIsInitialized_ = true;
 }
@@ -25,10 +41,19 @@ Button::~Button()
 
 void Button::Tick(float deltaSeconds)
 {
+	Press press = app_->GetMousePress(layout_.mouse);
+	state_ = GetState(press, state_);
+
+	if (state_ == State::RELEASED && clickEvent_)
+	{
+		clickEvent_();
+	}
 }
 
 void Button::Render()
 {
+	RenderManager2D::Get().DrawRoundRect(bound_.center, bound_.size.x, bound_.size.y, layout_.side, stateColors_.at(state_));
+	RenderManager2D::Get().DrawString(layout_.font, layout_.text, textPos_, layout_.textColor);
 }
 
 void Button::Release()
@@ -38,4 +63,84 @@ void Button::Release()
 	app_ = nullptr;
 
 	bIsInitialized_ = false;
+}
+
+Button::State Button::GetState(const Press& press, const State& state)
+{
+	if (!IsDetectMouseCursor())
+	{
+		if (state == State::PRESSED && press == Press::HELD)
+		{
+			return State::PRESSED;
+		}
+		else
+		{
+			return State::DISABLED;
+		}
+	}
+
+	if (state == State::DISABLED)
+	{
+		if (press == Press::HELD || press == Press::PRESSED)
+		{
+			return State::DISABLED;
+		}
+		else // press == Press::NONE || press == Press::RELEASED
+		{
+			return State::ENABLED;
+		}
+	}
+	else if (state == State::ENABLED)
+	{
+		if (press == Press::HELD || press == Press::PRESSED)
+		{
+			return State::PRESSED;
+		}
+		else
+		{
+			return State::ENABLED;
+		}
+	}
+	else if (state == State::PRESSED)
+	{
+		if (press == Press::RELEASED)
+		{
+			return State::RELEASED;
+		}
+		else if (press == Press::HELD || press == Press::PRESSED)
+		{
+			return State::PRESSED;
+		}
+		else // press == Press::NONE
+		{
+			return State::ENABLED;
+		}
+	}
+	else // state == State::RELEASED
+	{
+		if (press == Press::NONE || press == Press::RELEASED)
+		{
+			return State::ENABLED;
+		}
+		else // press == Press::HELD || press == Press::PRESSED
+		{
+			return State::PRESSED;
+		}
+	}
+
+	return State::DISABLED;
+}
+
+bool Button::IsDetectMouseCursor()
+{
+	GameMath::Vec2i currPos = app_->GetCurrMousePos();
+
+	GameMath::Vec2f screenSize;
+	app_->GetScreenSize<float>(screenSize.x, screenSize.y);
+	
+	Point2D mouseCursor;
+	mouseCursor.center.x = -screenSize.x * 0.5f + static_cast<float>(currPos.x);
+	mouseCursor.center.y = +screenSize.y * 0.5f - static_cast<float>(currPos.y);
+	
+	return bound_.Intersect(&mouseCursor);
 }
