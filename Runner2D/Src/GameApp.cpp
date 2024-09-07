bf8cbd7d3280741105d92d4b28ae3cf8713746ae
choijione <choijione02@gmail.com>
2024-09-07 14:07:08 +0900

    다양한 크기의 폰트 리소스를 로딩하는 기능 추가

--- Runner2D/Src/GameApp.cpp
+++ Runner2D/Src/GameApp.cpp
@@ -5,6 +5,7 @@
 #include "RenderManager2D.h"
 #include "ResourceManager.h"
 #include "Texture2D.h"
+#include "TTFont.h"
 
 #include "Background.h"
 #include "Box.h"
@@ -97,4 +98,12 @@ void GameApp::LoadResource()
 	
 	Atlas2D* atlas = ResourceManager::Get().Create<Atlas2D>(resourceRootPath_ + "Atlas\\Atlas.png", resourceRootPath_ + "Atlas\\Atlas.json", Texture2D::Filter::NEAREST);
 	ResourceManager::Get().Register("Atlas", atlas);
+
+	std::string fontPath = resourceRootPath_ + "Font\\Yolk.ttf";
+	std::array<int32_t, 4> fontSizes = { 16, 24, 32, 64 };
+	for (const auto& fontSize : fontSizes)
+	{
+		TTFont* font = ResourceManager::Get().Create<TTFont>(fontPath, 0, 9660, static_cast<float>(fontSize));
+		ResourceManager::Get().Register(GameUtils::PrintF("Font%d", fontSize), font);
+	}
 }
