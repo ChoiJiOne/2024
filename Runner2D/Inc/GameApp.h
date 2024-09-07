9274a74aa0565810c0df8b3a85f77d8b903bb7df
choijione <choijione02@gmail.com>
2024-09-08 07:37:53 +0900

    게임 앱의 상태를 나타내는 열거형 추가

--- Runner2D/Inc/GameApp.h
+++ Runner2D/Inc/GameApp.h
@@ -9,6 +9,16 @@ class Entity2D;
 class GameApp : public IApp
 {
 public:
+	enum class EStatus
+	{
+		READY = 0x00,
+		PLAY  = 0x01,
+		PAUSE = 0x02,
+		DONE  = 0x03,
+	};
+
+
+public:
 	GameApp();
 	virtual ~GameApp();
 
