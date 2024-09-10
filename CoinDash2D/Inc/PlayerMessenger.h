317bdb1f6be7ea10615d9e165152bf4672282d7c
choijione <choijione02@gmail.com>
2024-09-10 14:57:39 +0900

    이전 프로젝트(Runner2D)에서 사용하던 플레이어의 메시지를 출력하는 엔티티 추가
    - https://github.com/ChoiJiOne/Runner2D/blob/develop/Runner2D/Inc/PlayerMessenger.h
    - https://github.com/ChoiJiOne/Runner2D/blob/develop/Runner2D/Src/PlayerMessenger.cpp

---
+++ CoinDash2D/Inc/PlayerMessenger.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <string>
+
+#include "GameMath.h"
+
+#include "Entity2D.h"
+
+class Player;
+class TTFont;
+
+class PlayerMessenger : public Entity2D
+{
+public:
+	PlayerMessenger();
+	virtual ~PlayerMessenger();
+
+	DISALLOW_COPY_AND_ASSIGN(PlayerMessenger);
+
+	virtual void Tick(float deltaSeconds) override;
+	virtual void Render() override;
+	virtual void Release() override;
+
+	void Send(const std::wstring& message, const GameMath::Vec4f& color, float time);
+
+private:
+	void CalcMessagePosFromPlayer();
+
+private:
+	Player* player_ = nullptr;
+	TTFont* font_ = nullptr;
+
+	bool bIsDetectMessage_ = false;
+
+	std::wstring message_;
+	GameMath::Vec2f messageSize_;
+	GameMath::Vec2f messagePos_;
+	GameMath::Vec4f messageColor_;
+	float remainTime_ = 0.0f;
+	float messageTime_ = 0.0f;
+};
