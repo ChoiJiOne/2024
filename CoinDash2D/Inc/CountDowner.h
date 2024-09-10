b78349b2009a054bd784ae659e000efbfac44400
choijione <choijione02@gmail.com>
2024-09-11 07:54:15 +0900

    게임의 플레이 시간을 추적하는 카운트다운 엔티티 추가

---
+++ CoinDash2D/Inc/CountDowner.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Entity2D.h"
+
+class TTFont;
+class Player;
+
+class CountDowner : public Entity2D
+{
+public:
+	CountDowner();
+	virtual ~CountDowner();
+
+	DISALLOW_COPY_AND_ASSIGN(CountDowner);
+
+	virtual void Tick(float deltaSeconds) override;
+	virtual void Render() override;
+	virtual void Release() override;
+
+	float GetTime() const { return time_; }
+	void SetTime(float time) { time_ = time; }
+
+private:
+	Player* player_ = nullptr;
+	TTFont* font_ = nullptr;
+
+	float time_ = 0.0f;
+	int32_t remainTime_ = 0;
+
+	GameMath::Vec2f textCenter_;
+	GameMath::Vec2f textPos_;
+	GameMath::Vec4f textColor_;
+	GameMath::Vec2f textShadowPos_;
+	GameMath::Vec4f textShadowColor_;
+	std::wstring text_;
+};
