d72d8e7de912ebdab482b7af17e1b2fc1a38497d
choijione <choijione02@gmail.com>
2024-09-11 08:15:24 +0900

    플레이어가 획득한 코인의 개수를 보여주는 엔티티 추가

---
+++ CoinDash2D/Inc/CoinViewer.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Entity2D.h"
+
+class TTFont;
+class Player;
+
+class CoinViewer : public Entity2D
+{
+public:
+	CoinViewer();
+	virtual ~CoinViewer();
+
+	DISALLOW_COPY_AND_ASSIGN(CoinViewer);
+
+	virtual void Tick(float deltaSeconds) override;
+	virtual void Render() override;
+	virtual void Release() override;
+
+private:
+	Player* player_ = nullptr;
+	TTFont* font_ = nullptr;
+
+	int32_t numCoin_ = -1;
+
+	GameMath::Vec2f textCenter_;
+	GameMath::Vec2f textPos_;
+	GameMath::Vec4f textColor_;
+	GameMath::Vec2f textShadowPos_;
+	GameMath::Vec4f textShadowColor_;
+	std::wstring text_;
+};
