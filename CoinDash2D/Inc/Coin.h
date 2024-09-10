983c82a8ae0ed9b67634d91b7e2a47df9709cef6
choijione <choijione02@gmail.com>
2024-09-10 10:28:52 +0900

    플레이어가 획득할 코인 엔티티 추가

---
+++ CoinDash2D/Inc/Coin.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Collision2D.h"
+#include "Entity2D.h"
+
+class Player;
+class SpriteAnim2D;
+
+class Coin : public Entity2D
+{
+public:
+	Coin();
+	virtual ~Coin();
+
+	DISALLOW_COPY_AND_ASSIGN(Coin);
+
+	virtual void Tick(float deltaSeconds) override;
+	virtual void Render() override;
+	virtual void Release() override;
+	
+private:
+	Player* player_ = nullptr;
+
+	SpriteAnim2D* anim_ = nullptr;
+
+	Circle2D bound_;
+};
---
+++ CoinDash2D/Src/Coin.cpp
@@ -0,0 +1,54 @@
+#include "Assertion.h"
+#include "Atlas2D.h"
+#include "EntityManager.h"
+#include "IApp.h"
+#include "RenderManager2D.h"
+#include "ResourceManager.h"
+#include "SpriteAnim2D.h"
+
+#include "Coin.h"
+#include "Player.h"
+
+Coin::Coin()
+{
+	player_ = EntityManager::Get().GetByName<Player>("Player");
+
+	Atlas2D* atlas = ResourceManager::Get().GetByName<Atlas2D>("Atlas");
+
+	std::vector<std::string> clips = { "Coin_1", "Coin_2", "Coin_3", "Coin_4", "Coin_5", "Coin_6", "Coin_7", "Coin_8", "Coin_9", "Coin_10", "Coin_11", };
+	anim_ = ResourceManager::Get().Create<SpriteAnim2D>(atlas, clips, true, 1.0f);
+
+	bound_ = Circle2D(GameMath::Vec2f(100.0f, 100.0f), 15.0f);
+
+	bIsInitialized_ = true;
+}
+
+Coin::~Coin()
+{
+	if (bIsInitialized_)
+	{
+		Release();
+	}
+}
+
+void Coin::Tick(float deltaSeconds)
+{
+	anim_->Update(deltaSeconds);
+}
+
+void Coin::Render()
+{
+	RenderManager2D::Get().DrawSprite(anim_->GetAtlas(), anim_->GetCurrentClip(), bound_.center, 2.0f * bound_.radius, 2.0f * bound_.radius);
+}
+
+void Coin::Release()
+{
+	CHECK(bIsInitialized_);
+
+	ResourceManager::Get().Destroy(anim_);
+	anim_ = nullptr;
+
+	player_ = nullptr;
+
+	bIsInitialized_ = false;
+}
