535283b44862a34019d0f5eb88df708bd916b486
choijione <choijione02@gmail.com>
2024-10-03 18:00:37 +0900

    현재 테트로미노와 다음 테트로미노 생성 및 업데이트/렌더링 수행하는 코드 추가

--- Tetris2D/Src/TetrominoController.cpp
+++ Tetris2D/Src/TetrominoController.cpp
@@ -7,6 +7,14 @@
 
 TetrominoController::TetrominoController()
 {
+	board_ = EntityManager::Get().GetByName<Board>("Board");
+
+	startPos_ = board_->GetStartPos();
+	waitPos_ = Vec2f(150.0f, 100.0f);
+
+	currentTetromino_ = Tetromino::CreateRandom(startPos_, 27.0f, 30.0f, Vec4f(1.0f, 1.0f, 1.0f, 1.0f));
+	nextTetromino_ = Tetromino::CreateRandom(waitPos_, 27.0f, 30.0f, Vec4f(1.0f, 1.0f, 1.0f, 1.0f));
+
 	bIsInitialized_ = true;
 }
 
@@ -20,15 +28,31 @@ TetrominoController::~TetrominoController()
 
 void TetrominoController::Tick(float deltaSeconds)
 {
+	currentTetromino_->Tick(deltaSeconds);
+	nextTetromino_->Tick(deltaSeconds);
 }
 
 void TetrominoController::Render()
 {
+	currentTetromino_->Render();
+	nextTetromino_->Render();
 }
 
 void TetrominoController::Release()
 {
 	CHECK(bIsInitialized_);
+
+	if (nextTetromino_)
+	{
+		EntityManager::Get().Destroy(nextTetromino_);
+		nextTetromino_ = nullptr;
+	}
+
+	if (currentTetromino_)
+	{
+		EntityManager::Get().Destroy(currentTetromino_);
+		currentTetromino_ = nullptr;
+	}
 
 	board_ = nullptr;
 
