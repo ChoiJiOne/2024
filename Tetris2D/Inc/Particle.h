8f3abe3eae1f7886cb32e8a3e17e24b3670101c5
choijione <choijione02@gmail.com>
2024-10-09 15:22:48 +0900

    구현 계획 변경으로 열거형 및 카메라를 제외한 멤버 변수 삭제

--- Tetris2D/Inc/Particle.h
+++ Tetris2D/Inc/Particle.h
@@ -12,14 +12,6 @@ class Camera2D;
 class Particle : public IEntity2D
 {
 public:
-	enum class Status
-	{
-		WAIT      = 0x00,
-		EXPLOSION = 0x01,
-		DONE      = 0x02,
-	};
-
-public:
 	Particle();
 	virtual ~Particle();
 
@@ -29,15 +21,6 @@ public:
 	virtual void Render() override;
 	virtual void Release() override;
 
-	const Status GetStatus() const { return status_; }
-
 private:
 	Camera2D* camera_ = nullptr;
-
-	static const uint32_t MAX_PARTICLE_COUNT = 200;
-
-	uint32_t numParticleBlock_ = 0;
-	std::array<Block, MAX_PARTICLE_COUNT> particles_;
-
-	Status status_;
 };
