a566dbb93bc2b45406b23e025c90b374c9d5a5d3
choijione <choijione02@gmail.com>
2024-10-17 09:43:48 +0900

    게임 앱 내의 기존에 있던 게임 앱 상태 별 엔티티 목록 삭제

--- Tetris2D/Inc/GameApp.h
+++ Tetris2D/Inc/GameApp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <map>
+#include <memory>
 
 #include "IApp.h"
 
@@ -8,18 +9,10 @@ class MainCamera2D;
 class IEntity;
 class IEntity2D;
 class IEntityUI;
+class IGameScene;
 
 class GameApp : public IApp 
 {
-public:
-	enum class Status
-	{
-		TITLE    = 0x00,
-		GAMEPLAY = 0x01,
-		PAUSE    = 0x02,
-		GAMEOVER = 0x03,
-	};
-
 public:
 	GameApp();
 	virtual ~GameApp();
@@ -31,25 +24,14 @@ public:
 	virtual void Run() override;
 
 private:
-	struct StatusEntities
-	{
-		std::vector<IEntity*> updateEntities_;
-		std::vector<IEntity2D*> renderEntities_;
-		std::vector<IEntityUI*> uiEntities_;
-	};
-
 	void LoadResource();
-	void LoadTitleStatusEntities();
-	void LoadGamePlayStatusEntities();
 
-private:	
+private:
 	class EntityManager* entityMgr_ = nullptr;
-	class RenderManager2D* render2dMgr_ = nullptr;
 	class ResourceManager* resourceMgr_ = nullptr;
-	class UIManager* uiMgr_ = nullptr;
 
 	MainCamera2D* mainCamera_ = nullptr;
 
-	Status status_ = Status::TITLE;
-	std::map<Status, StatusEntities> statusEntities_;
+	IGameScene* currentScene_ = nullptr;
+	std::unique_ptr<class TitleScene2D> titleScene_ = nullptr;
 };
--- Tetris2D/Src/GameApp.cpp
+++ Tetris2D/Src/GameApp.cpp
@@ -8,28 +8,19 @@
 #include "TTFont.h"
 #include "UIManager.h"
 
-#include "Board.h"
 #include "GameApp.h"
 #include "MainCamera2D.h"
-#include "Messenger.h"
-#include "Next.h"
-#include "ParticleScheduler.h"
-#include "Tetromino.h"
-#include "TetrominoController.h"
+#include "TitleScene2D.h"
 
 GameApp::GameApp() : IApp("Tetris2D", 200, 200, 600, 800, false, false)
 {
 	entityMgr_ = EntityManager::GetPtr();
-	render2dMgr_ = RenderManager2D::GetPtr();
 	resourceMgr_ = ResourceManager::GetPtr();
-	uiMgr_ = UIManager::GetPtr();
 }
 
 GameApp::~GameApp()
 {
-	uiMgr_ = nullptr;
 	resourceMgr_ = nullptr;
-	render2dMgr_ = nullptr;
 	entityMgr_ = nullptr;
 }
 
@@ -39,9 +30,10 @@ void GameApp::Startup()
 
 	mainCamera_ = entityMgr_->Create<MainCamera2D>();
 	entityMgr_->Register("MainCamera", mainCamera_);
-	
-	LoadTitleStatusEntities();
-	LoadGamePlayStatusEntities();
+
+	titleScene_ = std::make_unique<TitleScene2D>();
+
+	currentScene_ = titleScene_.get();
 }
 
 void GameApp::Shutdown()
@@ -52,31 +44,23 @@ void GameApp::Shutdown()
 
 void GameApp::Run()
 {
+	currentScene_->Enter();
+
 	RunLoop(
 		[&](float deltaSeconds)
 		{
-			StatusEntities& statusEntities = statusEntities_.at(status_);
-
-			// 2D 엔티티 업데이트
-			for (auto& entity : statusEntities.updateEntities_)
-			{
-				entity->Tick(deltaSeconds);
-			}
-
-			// UI 엔티티 업데이트
-			IEntityUI** uiEntities = statusEntities.uiEntities_.data();
-			uint32_t uiEntityCount = static_cast<uint32_t>(statusEntities.uiEntities_.size());
-			uiMgr_->BatchTickUIEntity(uiEntities, uiEntityCount, deltaSeconds);
+			currentScene_->Tick(deltaSeconds);
 
 			BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
-
-			IEntity2D** renderEntities = statusEntities.renderEntities_.data();
-			uint32_t renderEntityCount = static_cast<uint32_t>(statusEntities.renderEntities_.size());
-
-			render2dMgr_->BatchRenderEntities(mainCamera_, renderEntities, renderEntityCount);
-			uiMgr_->BatchRenderUIEntity(uiEntities, uiEntityCount);
-			
+			currentScene_->Render();
 			EndFrame();
+
+			if (currentScene_->IsSceneSwitched())
+			{
+				currentScene_->Exit();
+				currentScene_ = currentScene_->GetSwitchScene();
+				currentScene_->Enter();
+			}
 		}
 	);
 }
@@ -93,76 +77,76 @@ void GameApp::LoadResource()
 	}
 }
 
-void GameApp::LoadTitleStatusEntities()
-{
-	TTFont* font32 = resourceMgr_->GetByName<TTFont>("Font32");
-	TTFont* font128 = resourceMgr_->GetByName<TTFont>("Font128");
+//void GameApp::LoadTitleStatusEntities()
+//{
+//	TTFont* font32 = resourceMgr_->GetByName<TTFont>("Font32");
+//	TTFont* font128 = resourceMgr_->GetByName<TTFont>("Font128");
+//
+//	TextUI* title = uiMgr_->CreateTextUI("Tetris2D\\Res\\UI\\Title.ui", font128);
+//	ButtonUI* startBtn = uiMgr_->CreateButtonUI("Tetris2D\\Res\\UI\\Start.ui", Mouse::LEFT, font32, [&]() { status_ = Status::GAMEPLAY; });
+//	ButtonUI* quitBtn = uiMgr_->CreateButtonUI("Tetris2D\\Res\\UI\\Quit.ui", Mouse::LEFT, font32, [&]() { bIsQuit_ = true; });
+//
+//	StatusEntities statusEntities;
+//	statusEntities.updateEntities_ = 
+//	{
+//		mainCamera_,
+//	};
+//	statusEntities.uiEntities_ =
+//	{ 
+//		title,
+//		startBtn,
+//		quitBtn,
+//	};
+//
+//	statusEntities_.insert({ Status::TITLE, statusEntities });
+//}
 
-	TextUI* title = uiMgr_->CreateTextUI("Tetris2D\\Res\\UI\\Title.ui", font128);
-	ButtonUI* startBtn = uiMgr_->CreateButtonUI("Tetris2D\\Res\\UI\\Start.ui", Mouse::LEFT, font32, [&]() { status_ = Status::GAMEPLAY; });
-	ButtonUI* quitBtn = uiMgr_->CreateButtonUI("Tetris2D\\Res\\UI\\Quit.ui", Mouse::LEFT, font32, [&]() { bIsQuit_ = true; });
-
-	StatusEntities statusEntities;
-	statusEntities.updateEntities_ = 
-	{
-		mainCamera_,
-	};
-	statusEntities.uiEntities_ =
-	{ 
-		title,
-		startBtn,
-		quitBtn,
-	};
-
-	statusEntities_.insert({ Status::TITLE, statusEntities });
-}
-
-void GameApp::LoadGamePlayStatusEntities()
-{
-	ParticleScheduler* particleScheduler = entityMgr_->Create<ParticleScheduler>();
-	entityMgr_->Register("ParticleScheduler", particleScheduler);
-
-	Next* next = entityMgr_->Create<Next>();
-	entityMgr_->Register("Next", next);
-	
-	Messenger* messenger = entityMgr_->Create<Messenger>();
-	entityMgr_->Register("Messenger", messenger);
-
-	Board* board = entityMgr_->Create<Board>(Vec2f(-50.0f, 0.0f), 30.0f, 10, 20);
-	entityMgr_->Register("Board", board);
-
-	TetrominoController* tetrominoController = entityMgr_->Create<TetrominoController>();
-	entityMgr_->Register("TetrominoController", tetrominoController);
-
-	TTFont* font32 = resourceMgr_->GetByName<TTFont>("Font32");
-	TextUI* nextText = uiMgr_->CreateTextUI("Tetris2D\\Res\\UI\\Next.ui", font32);
-	TextUI* scoreText = uiMgr_->CreateTextUI("Tetris2D\\Res\\UI\\ScoreText.ui", font32);
-	PanelUI* score = uiMgr_->CreatePanelUI("Tetris2D\\Res\\UI\\ScorePanel.ui", font32);
-	entityMgr_->Register("Score", score);
-
-	StatusEntities statusEntities;
-	statusEntities.updateEntities_ =
-	{
-		mainCamera_,
-		tetrominoController,
-		board,
-		particleScheduler,
-		messenger,
-	};
-	statusEntities.renderEntities_ =
-	{ 
-		board,
-		next,
-		tetrominoController,
-		particleScheduler,
-		messenger,
-	};
-	statusEntities.uiEntities_ =
-	{
-		nextText,
-		scoreText,
-		score,
-	};
-
-	statusEntities_.insert({ Status::GAMEPLAY, statusEntities });
-}
+//void GameApp::LoadGamePlayStatusEntities()
+//{
+//	ParticleScheduler* particleScheduler = entityMgr_->Create<ParticleScheduler>();
+//	entityMgr_->Register("ParticleScheduler", particleScheduler);
+//
+//	Next* next = entityMgr_->Create<Next>();
+//	entityMgr_->Register("Next", next);
+//	
+//	Messenger* messenger = entityMgr_->Create<Messenger>();
+//	entityMgr_->Register("Messenger", messenger);
+//
+//	Board* board = entityMgr_->Create<Board>(Vec2f(-50.0f, 0.0f), 30.0f, 10, 20);
+//	entityMgr_->Register("Board", board);
+//
+//	TetrominoController* tetrominoController = entityMgr_->Create<TetrominoController>();
+//	entityMgr_->Register("TetrominoController", tetrominoController);
+//
+//	TTFont* font32 = resourceMgr_->GetByName<TTFont>("Font32");
+//	TextUI* nextText = uiMgr_->CreateTextUI("Tetris2D\\Res\\UI\\Next.ui", font32);
+//	TextUI* scoreText = uiMgr_->CreateTextUI("Tetris2D\\Res\\UI\\ScoreText.ui", font32);
+//	PanelUI* score = uiMgr_->CreatePanelUI("Tetris2D\\Res\\UI\\ScorePanel.ui", font32);
+//	entityMgr_->Register("Score", score);
+//
+//	StatusEntities statusEntities;
+//	statusEntities.updateEntities_ =
+//	{
+//		mainCamera_,
+//		tetrominoController,
+//		board,
+//		particleScheduler,
+//		messenger,
+//	};
+//	statusEntities.renderEntities_ =
+//	{ 
+//		board,
+//		next,
+//		tetrominoController,
+//		particleScheduler,
+//		messenger,
+//	};
+//	statusEntities.uiEntities_ =
+//	{
+//		nextText,
+//		scoreText,
+//		score,
+//	};
+//
+//	statusEntities_.insert({ Status::GAMEPLAY, statusEntities });
+//}
