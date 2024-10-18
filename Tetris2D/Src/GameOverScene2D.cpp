12d9f5b37b33a2d56ea7c5807fa27a8f2650b3dd
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2024-10-18 13:05:16 +0900

    2D 게임 오버 씬에 기본적인 엔티티 및 업데이트/렌더링을 수행하는 기능 추가

--- Tetris2D/Src/GameOverScene2D.cpp
+++ Tetris2D/Src/GameOverScene2D.cpp
@@ -15,6 +15,7 @@
 #include "Board.h"
 #include "GameOverScene2D.h"
 #include "GamePlayScene2D.h"
+#include "GameTitleScene2D.h"
 #include "MainCamera2D.h"
 #include "Messenger.h"
 #include "Next.h"
@@ -22,3 +23,67 @@
 #include "ParticleScheduler.h"
 #include "Tetromino.h"
 #include "TetrominoController.h"
+
+GameOverScene2D::GameOverScene2D()
+{
+	mainCamera_ = entityMgr_->GetByName<MainCamera2D>("MainCamera");
+
+	ParticleScheduler* particleScheduler = entityMgr_->GetByName<ParticleScheduler>("ParticleScheduler");
+	Next* next = entityMgr_->GetByName<Next>("Next");
+	Board* board = entityMgr_->GetByName<Board>("Board");
+	TetrominoController* tetrominoController = entityMgr_->GetByName<TetrominoController>("TetrominoController");
+
+	renderEntities_.push_back(board);
+	renderEntities_.push_back(next);
+	renderEntities_.push_back(tetrominoController);
+	renderEntities_.push_back(particleScheduler);
+}
+
+GameOverScene2D::~GameOverScene2D()
+{
+}
+
+void GameOverScene2D::Tick(float deltaSeconds)
+{
+	for (auto& entity : updateEntities_)
+	{
+		entity->Tick(deltaSeconds);
+	}
+
+	for (auto& uiEntity : uiEntities_)
+	{
+		uiEntity->Tick(deltaSeconds);
+	}
+}
+
+void GameOverScene2D::Render()
+{
+	renderStateMgr_->BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
+	{// 2D ¿£Æ¼Æ¼ ·»´õ¸µ
+		render2dMgr_->Begin(mainCamera_);
+		{
+			for (auto& entity : renderEntities_)
+			{
+				entity->Render();
+			}
+		}
+		render2dMgr_->End();
+	}
+	{// UI ·»´õ¸µ
+		IEntityUI** uiEntities = uiEntities_.data();
+		uint32_t uiEntityCount = static_cast<uint32_t>(uiEntities_.size());
+		uiMgr_->BatchRenderUIEntity(uiEntities, uiEntityCount);
+	}
+	renderStateMgr_->EndFrame();
+}
+
+void GameOverScene2D::Enter()
+{
+	bIsEnter_ = true;
+}
+
+void GameOverScene2D::Exit()
+{
+	bIsSwitched_ = false;
+	bIsEnter_ = false;
+}
