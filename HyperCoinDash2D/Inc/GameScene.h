8ac24e4303f8791375afd3faf83238aab7a8fe1f
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2024-10-29 10:37:11 +0900

    게임 씬이 게임 씬 인터페이스를 상속받도록 수정

--- HyperCoinDash2D/Inc/GameScene.h
+++ HyperCoinDash2D/Inc/GameScene.h
@@ -1,12 +1,17 @@
 #pragma once
 
-#include "IGameScene2D.h"
+#include "IGameScene.h"
 
-class GameScene : public IGameScene2D
+class GameScene : public IGameScene
 {
 public:
 	GameScene();
 	virtual ~GameScene();
 
 	DISALLOW_COPY_AND_ASSIGN(GameScene);
+
+	virtual void Tick(float deltaSeconds) override;
+	virtual void Render() override;
+	virtual void Enter() override;
+	virtual void Exit() override;
 };
--- HyperCoinDash2D/Src/GameScene.cpp
+++ HyperCoinDash2D/Src/GameScene.cpp
@@ -1,13 +1,36 @@
 #include "Assertion.h"
-#include "EntityManager.h"
+#include "RenderStateManager.h"
 
 #include "GameScene.h"
 
 GameScene::GameScene()
 {
-	mainCamera_ = EntityManager::GetRef().GetByName<Camera2D>("MainCamera");
 }
 
 GameScene::~GameScene()
 {
+}
+
+void GameScene::Tick(float deltaSeconds)
+{
+}
+
+void GameScene::Render()
+{
+	RenderStateManager::GetRef().BeginFrame(1.0f, 0.0f, 0.0f, 1.0f);
+	{
+
+	}
+	RenderStateManager::GetRef().EndFrame();
+}
+
+void GameScene::Enter()
+{
+	bIsEnter_ = true;
+}
+
+void GameScene::Exit()
+{
+	bIsSwitched_ = false;
+	bIsEnter_ = false;
 }
