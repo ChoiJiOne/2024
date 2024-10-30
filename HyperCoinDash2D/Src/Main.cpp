d212c59344ea6a784aff7353b7b88f34d3bc4402
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2024-10-30 12:10:10 +0900

    FreeType 테스트 코드 추가

--- HyperCoinDash2D/Src/Main.cpp
+++ HyperCoinDash2D/Src/Main.cpp
@@ -2,9 +2,21 @@
 
 #include <SDL3/SDL.h>
 
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
 int main(int argc, char* argv[])
 {
     SDL_Init(SDL_INIT_VIDEO);
+
+	FT_Library ft;
+	if (FT_Init_FreeType(&ft))
+	{
+		std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+		return -1;
+	}
+
+
     SDL_Quit();
     return 0;
 }
