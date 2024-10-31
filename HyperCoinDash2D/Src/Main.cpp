81df874fdc159c3915646176eda7b72c08db4e30
choijione <choijione02@gmail.com>
2024-10-31 14:53:22 +0900

    윈도우 창 생성 및 아무 동작도 수행하지 않는 게임 루프 추가

--- HyperCoinDash2D/Src/Main.cpp
+++ HyperCoinDash2D/Src/Main.cpp
@@ -1,7 +1,38 @@
 #include <cstdint>
 #include <Windows.h>
 
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+
+const uint32_t WINDOW_WIDTH = 1000;
+const uint32_t WINDOW_HEIGHT = 800;
+
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "HyperCoinDash2D", nullptr, nullptr);
+
+	glfwMakeContextCurrent(window);
+	
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+			glfwSetWindowShouldClose(window, true);
+
+		glfwSwapBuffers(window);
+	}
+	glfwDestroyWindow(window);
+	window = nullptr;
+
+	glfwTerminate();
+
 	return 0;
 }
