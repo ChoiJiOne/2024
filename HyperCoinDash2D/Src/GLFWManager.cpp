600e321b6ca6fe1e12fad670369d5a3a82049754
choijione <choijione02@gmail.com>
2024-11-01 16:52:22 +0900

    GLFW 매니저 내부에 어션설을 이용해서 검사를 수행

--- HyperCoinDash2D/Src/GLFWManager.cpp
+++ HyperCoinDash2D/Src/GLFWManager.cpp
@@ -1,3 +1,4 @@
+#include "GLFWAssertion.h"
 #include "GLFWManager.h"
 #include "MemoryAlloc.h"
 
@@ -88,15 +89,16 @@ void GLFWManager::Startup(int32_t width, int32_t height, const char* title)
 	allocator.reallocate = MemoryRealloc;
 	allocator.deallocate = MemoryFree;
 
-	glfwInitAllocator(&allocator);
-	glfwInit();
+	GLFW_API_CHECK(glfwInitAllocator(&allocator));
+	GLFW_EXP_CHECK(glfwInit());
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, GL_MAJOR_VERSION);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, GL_MINOR_VERSION);
-	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFW_API_CHECK(glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, GL_MAJOR_VERSION));
+	GLFW_API_CHECK(glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, GL_MINOR_VERSION));
+	GLFW_API_CHECK(glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE));
+	GLFW_API_CHECK(glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE));
 
 	mainWindow_ = glfwCreateWindow(width, height, title, nullptr, nullptr);
+	GLFW_EXP_CHECK(mainWindow_ != nullptr);
 }
 
 void GLFWManager::Shutdown()
