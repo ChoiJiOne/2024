f50ee27ab108083a831f2c567c31fe2fd83ca08a
choijione <choijione02@gmail.com>
2024-11-01 16:28:19 +0900

    DEBUG_MODE와 RELWITHDEBINFO_MODE에서 동작하는 GLFW API 용 어션설 매크로 추가

--- HyperCoinDash2D/Inc/GLFWAssertion.h
+++ HyperCoinDash2D/Inc/GLFWAssertion.h
@@ -5,35 +5,43 @@
 
 
 #if defined(DEBUG_MODE) || defined(RELWITHDEBINFO_MODE)
-#ifndef GLFW_CHECK
-#define GLFW_CHECK(EXP)\
+/** 이 매크로는 GLFW API의 호출 결과 평가식을 검사합니다. */
+
+/**
+ * 이 매크로는 GLFW API의 호출 결과 평가식을 검사합니다.
+ * ex) 
+ * GLFWwindow* window = glfwCreateWindow(...);
+ * GLFW_EXP_CHECK(window != nullptr);
+ */
+#ifndef GLFW_EXP_CHECK
+#define GLFW_EXP_CHECK(EXP)\
 {\
 	if (!(bool)(EXP))\
 	{\
-		DebugPrintF("\nAssertion check point failed!\n>File : %s\n>Line : %d\n>Function: %s\n>Expression : %s\n", __FILE__, __LINE__, __FUNCTION__, #EXP);\
+		DebugPrintF("\nAssertion check point failed!\n> File: %s\n> Line: %d\n> Function: %s\n> Expression: %s\n", __FILE__, __LINE__, __FUNCTION__, #EXP);\
+		DebugPrintF("> Code: %d\n> Error: %s\n> Description: %s\n\n", GLFWManager::GetRef().GetErrorCode(),  GLFWManager::GetRef().GetErrorMessage(),  GLFWManager::GetRef().GetErrorDescription());\
 		__debugbreak();\
 	}\
 }
 #endif
+/**
+ * 이 매크로는 GLFW API의 API 호출 결과를 검사합니다. 주로, 반환 값이 없는 API를 대상으로 합니다.
+ * ex)
+ * GLFW_API_CHECK(glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, GL_MAJOR_VERSION));
+ */
 
-//#ifndef GLFW_API_CHECK
-//#define GLFW_API_CHECK(EXP)
-//{
-//
-//}
-//#endif
-
-
-
-
-
-
+#ifndef GLFW_API_CHECK
+#define GLFW_API_CHECK(EXP)\
+{\
+	EXP;\
+	if (GLFWManager::GetRef().IsDetectError())\
+	{\
+		DebugPrintF("\nAssertion check point failed!\n> File: %s\n> Line: %d\n> Function: %s\n> Expression: %s\n", __FILE__, __LINE__, __FUNCTION__, #EXP);\
+		DebugPrintF("> Code: %d\n> Error: %s\n> Description: %s\n\n", GLFWManager::GetRef().GetErrorCode(),  GLFWManager::GetRef().GetErrorMessage(),  GLFWManager::GetRef().GetErrorDescription());\
+		__debugbreak();\
+	}\
+}
+#endif
 #else // defined(RELEASE_MODE) || defined(MINSIZEREL_MODE)
-
-
-
-
-
-
 
 #endif
