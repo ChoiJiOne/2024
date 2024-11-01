438657a687d05916bc882d2d92b39cfb0ea341ca
choijione <choijione02@gmail.com>
2024-11-01 16:49:15 +0900

    GLFW API 용 어션설 매크로가 RELEASE_MODE와 MINSIZEREL_MODE에서 동작할 때는 메시지 박스를 띄우도록 수정

--- HyperCoinDash2D/Inc/GLFWAssertion.h
+++ HyperCoinDash2D/Inc/GLFWAssertion.h
@@ -1,12 +1,11 @@
 #pragma once
 
+#include <Windows.h>
+
 #include "GLFWManager.h"
 #include "Utils.h"
 
-
 #if defined(DEBUG_MODE) || defined(RELWITHDEBINFO_MODE)
-/** 이 매크로는 GLFW API의 호출 결과 평가식을 검사합니다. */
-
 /**
  * 이 매크로는 GLFW API의 호출 결과 평가식을 검사합니다.
  * ex) 
@@ -29,7 +28,6 @@
  * ex)
  * GLFW_API_CHECK(glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, GL_MAJOR_VERSION));
  */
-
 #ifndef GLFW_API_CHECK
 #define GLFW_API_CHECK(EXP)\
 {\
@@ -43,5 +41,36 @@
 }
 #endif
 #else // defined(RELEASE_MODE) || defined(MINSIZEREL_MODE)
-
+/**
+ * 이 매크로는 GLFW API의 호출 결과 평가식을 검사합니다.
+ * ex)
+ * GLFWwindow* window = glfwCreateWindow(...);
+ * GLFW_EXP_CHECK(window != nullptr);
+ */
+#ifndef GLFW_EXP_CHECK
+#define GLFW_EXP_CHECK(EXP)\
+{\
+	if (!(bool)(EXP))\
+	{\
+		std::string message = PrintF("> Code: %d\n> Error: %s\n> Description: %s", GLFWManager::GetRef().GetErrorCode(),  GLFWManager::GetRef().GetErrorMessage(),  GLFWManager::GetRef().GetErrorDescription());\
+		MessageBox(NULL, message.c_str(), "Assertion check point failed!", MB_OK);\
+	}\
+}
+#endif
+/**
+ * 이 매크로는 GLFW API의 API 호출 결과를 검사합니다. 주로, 반환 값이 없는 API를 대상으로 합니다.
+ * ex)
+ * GLFW_API_CHECK(glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, GL_MAJOR_VERSION));
+ */
+#ifndef GLFW_API_CHECK
+#define GLFW_API_CHECK(EXP)\
+{\
+	EXP;\
+	if (GLFWManager::GetRef().IsDetectError())\
+	{\
+		std::string message = PrintF("> Code: %d\n> Error: %s\n> Description: %s", GLFWManager::GetRef().GetErrorCode(),  GLFWManager::GetRef().GetErrorMessage(),  GLFWManager::GetRef().GetErrorDescription());\
+		MessageBox(NULL, message.c_str(), "Assertion check point failed!", MB_OK); \
+	}\
+}
+#endif
 #endif
