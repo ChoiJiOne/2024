159ea8d19abd0896cae597ad9a1fcaf9f0d1f965
choijione <choijione02@gmail.com>
2024-11-05 09:59:21 +0900

    씬을 생성 및 파괴하는 기능 추가

--- HyperCoinDash2D/Inc/Scene/SceneManager.h
+++ HyperCoinDash2D/Inc/Scene/SceneManager.h
@@ -23,6 +23,39 @@ public:
 	/** 씬 매니저의 싱글턴 객체 포인터를 얻습니다. */
 	static SceneManager* GetPtr();
 
+	/** 씬을 생성합니다. */
+	template <typename TScene, typename... Args>
+	TScene* Create(Args&&... args)
+	{
+		if (!(0 <= sceneSize_ && sceneSize_ < MAX_ENTITY_SIZE))
+		{
+			return nullptr;
+		}
+
+		int32_t sceneID = -1;
+		for (uint32_t index = 0; index < sceneSize_; ++index)
+		{
+			if (!scenes_[index] && !usages_[index])
+			{
+				sceneID = static_cast<int32_t>(index);
+				break;
+			}
+		}
+
+		if (sceneID == -1)
+		{
+			sceneID = sceneSize_++;
+		}
+
+		usages_[sceneID] = true;
+		scenes_[sceneID] = std::make_unique<TScene>(args...);
+
+		return reinterpret_cast<TScene*>(scenes_[sceneID].get());
+	}
+
+	/** 생성된 씬을 파괴합니다. */
+	void Destroy(const IGameScene* scene);
+
 private:
 	/** GameApp에서 SceneManager의 내부에 접근할 수 있도록 설정. */
 	friend class GameApp;
@@ -40,4 +73,14 @@ private:
 private:
 	/** 씬 매니저의 싱글턴 객체입니다. */
 	static SceneManager singleton_;
+
+	/** 씬 매니저에서 관리하는 씬의 최대 개수입니다. */
+	static const uint32_t MAX_SCENE_SIZE = 10;
+
+	/** 씬 매니저의 씬 크기입니다. 최대 MAX_SCENE_SIZE개 까지 지원합니다. */
+	uint32_t sceneSize_ = 0;
+
+	/** 씬과 해당 씬의 사용 여부입니다. */
+	std::array<std::unique_ptr<IGameScene>, MAX_SCENE_SIZE> scenes_;
+	std::array<bool, MAX_SCENE_SIZE> usages_;
 };
--- HyperCoinDash2D/Src/Scene/SceneManager.cpp
+++ HyperCoinDash2D/Src/Scene/SceneManager.cpp
@@ -13,6 +13,41 @@ SceneManager* SceneManager::GetPtr()
 	return &singleton_;
 }
 
+void SceneManager::Destroy(const IGameScene* scene)
+{
+	int32_t sceneID = -1;
+	for (uint32_t index = 0; index < sceneSize_; ++index)
+	{
+		IGameScene* scenePtr = scenes_[index].get();
+		if (scene == scenePtr)
+		{
+			sceneID = static_cast<int32_t>(index);
+			break;
+		}
+	}
+
+	if (sceneID == -1)
+	{
+		return; // 해당 씬은 이미 할당 해제 되었거나, 씬 매니저에 의해 할당 해제된 엔티티입니다.
+	}
+
+	if (scenes_[sceneID])
+	{
+		scenes_[sceneID].reset();
+		usages_[sceneID] = false;
+	}
+}
+
 void SceneManager::Shutdown()
 {
+	for (std::size_t index = 0; index < sceneSize_; ++index)
+	{
+		if (scenes_[index])
+		{
+			scenes_[index].reset();
+			usages_[index] = false;
+		}
+	}
+
+	sceneSize_ = 0;
 }
