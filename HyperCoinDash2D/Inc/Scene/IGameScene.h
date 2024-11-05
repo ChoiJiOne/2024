4d3a6f740d0ed5de076f170999d8e6219bd62f28
choijione <choijione02@gmail.com>
2024-11-05 09:28:46 +0900

    게임 씬 인터페이스 추가

---
+++ HyperCoinDash2D/Inc/Scene/IGameScene.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+
+#include "Macro.h"
+
+/** 게임 씬 인터페이스입니다. */
+class IGameScene
+{
+public:
+	IGameScene() {}
+	virtual ~IGameScene() {}
+
+	DISALLOW_COPY_AND_ASSIGN(IGameScene);
+
+	virtual void Tick(float deltaSeconds) = 0;
+	virtual void Render() = 0;
+	virtual void Enter() = 0;
+	virtual void Exit() = 0;
+
+	bool IsEnter() { return bIsEnter_; }
+	bool IsSceneSwitched() { return bIsSwitched_; }
+	IGameScene* GetSwitchScene() { return switchScene_; }
+
+protected:
+	bool bIsEnter_ = false;
+	bool bIsSwitched_ = false;
+	IGameScene* switchScene_ = nullptr;
+};
