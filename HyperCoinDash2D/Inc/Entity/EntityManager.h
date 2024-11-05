fb4c7f0784f14e73c1a808e3a52966858911f06c
choijione <choijione02@gmail.com>
2024-11-05 09:09:50 +0900

    엔티티를 생성/파괴하는 기능 추가

--- HyperCoinDash2D/Inc/Entity/EntityManager.h
+++ HyperCoinDash2D/Inc/Entity/EntityManager.h
@@ -24,6 +24,39 @@ public:
 	/** 엔티티 매니저의 싱글턴 포인터를 얻습니다. */
 	static EntityManager* GetPtr();
 
+	/** 엔티티를 생성합니다. */
+	template <typename TEntity, typename... Args>
+	TEntity* Create(Args&&... args)
+	{
+		if (!(0 <= entitySize_ && entitySize_ < MAX_ENTITY_SIZE))
+		{
+			return nullptr;
+		}
+
+		int32_t entityID = -1;
+		for (uint32_t index = 0; index < entitySize_; ++index)
+		{
+			if (!entities_[index] && !usages_[index])
+			{
+				entityID = static_cast<int32_t>(index);
+				break;
+			}
+		}
+
+		if (entityID == -1)
+		{
+			entityID = entitySize_++;
+		}
+
+		usages_[entityID] = true;
+		entities_[entityID] = std::make_unique<TEntity>(args...);
+
+		return reinterpret_cast<TEntity*>(entities_[entityID].get());
+	}
+
+	/** 생성한 엔티티를 파괴합니다. */
+	void Destroy(const IEntity* entity);
+
 private:
 	/** GameApp에서 EntityManager의 내부에 접근할 수 있도록 설정 */
 	friend class GameApp;
@@ -41,4 +74,14 @@ private:
 private:
 	/** 엔티티 매니저의 싱글턴 객체입니다. */
 	static EntityManager singleton_;
+
+	/** 엔티티 매니저에서 관리하는 엔티티의 최대 개수입니다. */
+	static const uint32_t MAX_ENTITY_SIZE = 300;
+
+	/** 엔티티 매니저의 엔티티 크기입니다. 최대 MAX_ENTITY_SIZE개 까지 지원합니다. */
+	uint32_t entitySize_ = 0;
+
+	/** 엔티티와 해당 엔티티의 사용 여부입니다. */
+	std::array<std::unique_ptr<IEntity>, MAX_ENTITY_SIZE> entities_;
+	std::array<bool, MAX_ENTITY_SIZE> usages_;
 };
--- HyperCoinDash2D/Src/Entity/EntityManager.cpp
+++ HyperCoinDash2D/Src/Entity/EntityManager.cpp
@@ -13,6 +13,51 @@ EntityManager* EntityManager::GetPtr()
 	return &singleton_;
 }
 
+void EntityManager::Destroy(const IEntity* entity)
+{
+	int32_t entityID = -1;
+	for (uint32_t index = 0; index < entitySize_; ++index)
+	{
+		IEntity* entityPtr = entities_[index].get();
+		if (entity == entityPtr)
+		{
+			entityID = static_cast<int32_t>(index);
+			break;
+		}
+	}
+
+	if (entityID == -1)
+	{
+		return; // 해당 엔티티는 이미 할당 해제 되었거나, 엔티티 매니저에 의해 
+	}
+
+	if (entities_[entityID])
+	{
+		if (entities_[entityID]->IsInitialized())
+		{
+			entities_[entityID]->Release();
+		}
+
+		entities_[entityID].reset();
+		usages_[entityID] = false;
+	}
+}
+
 void EntityManager::Shutdown()
 {
+	for (std::size_t index = 0; index < entitySize_; ++index)
+	{
+		if (entities_[index])
+		{
+			if (entities_[index]->IsInitialized())
+			{
+				entities_[index]->Release();
+			}
+
+			entities_[index].reset();
+			usages_[index] = false;
+		}
+	}
+
+	entitySize_ = 0;
 }
