776130c5b43a81452837062e81635a7592252a4f
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2024-11-05 11:46:14 +0900

    ImGui를 이용해서 FPS 출력 테스트 코드 추가

--- HyperCoinDash2D/Src/Scene/GameDevScene.cpp
+++ HyperCoinDash2D/Src/Scene/GameDevScene.cpp
@@ -1,3 +1,6 @@
+#include <glad/glad.h>
+#include <imgui.h>
+
 #include "GL/GLManager.h"
 #include "Scene/GameDevScene.h"
 #include "Utils/Assertion.h"
@@ -12,6 +15,9 @@ GameDevScene::~GameDevScene()
 
 void GameDevScene::Tick(float deltaSeconds)
 {
+	ImGui::Begin("FPS");
+	ImGui::Text("FPS: %f", 1.0f / deltaSeconds);
+	ImGui::End();
 }
 
 void GameDevScene::Render()
