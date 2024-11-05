5869322d73fe53ad0139dde242f99bce0e6c1921
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2024-11-05 12:13:28 +0900

    파일을 읽어 파일 내용을 벡터에 저장하는 기능 추가

--- HyperCoinDash2D/Inc/Utils/Utils.h
+++ HyperCoinDash2D/Inc/Utils/Utils.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 /**
  * DEBUG 창에 포멧팅된 문자열을 출력합니다.
@@ -13,3 +14,9 @@ std::string PrintF(const char* format, ...);
 
 /** 표준 문자열 기반의 문자열 포멧팅을 수행합니다. */
 std::wstring PrintF(const wchar_t* format, ...);
+
+/** 파일을 읽어 벡터에 저장합니다. */
+bool ReadFile(const std::string& path, std::vector<uint8_t>& outBuffer, std::string& outErrMsg);
+
+/** 파일을 읽어 벡터에 저장합니다. */
+bool ReadFile(const std::wstring& path, std::vector<uint8_t>& outBuffer, std::string& outErrMsg);
--- HyperCoinDash2D/Src/Utils/Utils.cpp
+++ HyperCoinDash2D/Src/Utils/Utils.cpp
@@ -46,3 +46,72 @@ std::wstring PrintF(const wchar_t* format, ...)
 
 	return std::wstring(buffer, size);
 }
+
+#if defined(WIN32) || defined(WIN64)
+const char* GetWindowsErrMessage()
+{
+	static const uint32_t MAX_BUFFER_SIZE = 1024;
+	static char buffer[MAX_BUFFER_SIZE];
+
+	uint32_t size = ::FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM, nullptr, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), buffer, MAX_BUFFER_SIZE, nullptr);
+	return buffer;
+}
+#endif
+
+bool ReadFile(const std::string& path, std::vector<uint8_t>& outBuffer, std::string& outErrMsg)
+{
+	HANDLE file = ::CreateFileA(path.c_str(), GENERIC_READ, FILE_SHARE_READ, nullptr, OPEN_EXISTING, 0, nullptr);
+	if (file == INVALID_HANDLE_VALUE)
+	{
+		outErrMsg = GetWindowsErrMessage();
+		return false;
+	}
+
+	DWORD fileSize = ::GetFileSize(file, nullptr);
+	outBuffer.resize(fileSize);
+
+	DWORD bytesRead;
+	if (!::ReadFile(file, outBuffer.data(), fileSize, &bytesRead, nullptr))
+	{
+		outErrMsg = GetWindowsErrMessage();
+		::CloseHandle(file);
+		return false;
+	}
+
+	if (!::CloseHandle(file))
+	{
+		outErrMsg = GetWindowsErrMessage();
+		return false;
+	}
+	
+	return true;
+}
+
+bool ReadFile(const std::wstring& path, std::vector<uint8_t>& outBuffer, std::string& outErrMsg)
+{
+	HANDLE file = ::CreateFileW(path.c_str(), GENERIC_READ, FILE_SHARE_READ, nullptr, OPEN_EXISTING, 0, nullptr);
+	if (file == INVALID_HANDLE_VALUE)
+	{
+		outErrMsg = GetWindowsErrMessage();
+		return false;
+	}
+
+	DWORD fileSize = ::GetFileSize(file, nullptr);
+	outBuffer.resize(fileSize);
+
+	DWORD bytesRead;
+	if (!::ReadFile(file, outBuffer.data(), fileSize, &bytesRead, nullptr))
+	{
+		outErrMsg = GetWindowsErrMessage();
+		::CloseHandle(file);
+		return false;
+	}
+
+	if (!::CloseHandle(file))
+	{
+		outErrMsg = GetWindowsErrMessage();
+		return false;
+	}
+
+	return true;
+}
