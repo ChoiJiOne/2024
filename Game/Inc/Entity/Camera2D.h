6f9182ba4d8c1fdc5a0730f793ef96eecb30a83b
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2024-11-06 14:06:56 +0900

    2D 카메라의 프로퍼티에 대한 Getter 추가

--- Game/Inc/Entity/Camera2D.h
+++ Game/Inc/Entity/Camera2D.h
@@ -23,6 +23,13 @@ public:
 	/** 2D 카메라 엔티티의 초기화를 해제합니다. */
 	virtual void Release() override;
 
+	/** 2D 카메라 엔티티의 프로퍼티에 대한 Getter 입니다. */
+	const glm::vec2& GetCenter() const { return center_; }
+	const glm::vec2& GetSize() const { return size_; }
+	const float& GetWidth() const { return size_.x; }
+	const float& GetHeight() const { return size_.y; }
+	const glm::mat4& GetOrtho() const { return ortho_; }
+
 protected:
 	/** 2D 카메라의 위치와 크기를 기반으로 직교 투영 행렬을 계산합니다. */
 	glm::mat4 CalculateOrtho(const glm::vec2& center, const glm::vec2& size, float zNear = -1.0f, float zFar = 1.0f);
