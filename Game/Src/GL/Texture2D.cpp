7406e4f71372bad25383e01d0e3b5979255065c9
choijione <choijione02@gmail.com>
2024-11-08 07:58:49 +0900

    텍스처 래핑 형식을 CLAMP_TO_EDGE에서 CLAMP_TO_BORDER로 수정

--- Game/Src/GL/Texture2D.cpp
+++ Game/Src/GL/Texture2D.cpp
@@ -70,10 +70,13 @@ uint32_t Texture2D::CreateTextureFromImage(const std::string& path, const EFilte
 	const void* bufferPtr = reinterpret_cast<const void*>(buffer.data());
 	uint32_t textureID = 0;
 
+	float borderColor[] = { 0.0f, 0.0f, 0.0f, 0.0f };
+
 	GL_API_CHECK(glGenTextures(1, &textureID));
 	GL_API_CHECK(glBindTexture(GL_TEXTURE_2D, textureID));
-	GL_API_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
-	GL_API_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
+	GL_API_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER));
+	GL_API_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER));
+	GL_API_CHECK(glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor));
 	GL_API_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, static_cast<GLint>(filter)));
 	GL_API_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, static_cast<GLint>(filter)));
 	GL_API_CHECK(glTexImage2D(GL_TEXTURE_2D, 0, format, width_, height_, 0, format, GL_UNSIGNED_BYTE, bufferPtr));
