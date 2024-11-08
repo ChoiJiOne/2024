ab91d79bf89876c2663b75668b5c9460b9fa9f50
choijione <choijione02@gmail.com>
2024-11-09 07:51:56 +0900

    2D AABB 끼리의 충돌 처리 기능 추가

--- Game/Src/Physic/Rect2D.cpp
+++ Game/Src/Physic/Rect2D.cpp
@@ -11,9 +11,61 @@
 #include "Physic/Rect2D.h"
 #include "Utils/Assertion.h"
 
+/** AABB客 AABB 尝府狼 面倒 贸府 */
+bool IsCollision(const Rect2D* lhs, const Rect2D* rhs)
+{
+	glm::vec2 minPos0 = lhs->GetMin();
+	glm::vec2 maxPos0 = lhs->GetMax();
+	glm::vec2 minPos1 = rhs->GetMin();
+	glm::vec2 maxPos1 = rhs->GetMax();
+
+	bool bIsOverlapX = ((minPos1.x <= maxPos0.x) && (minPos0.x <= maxPos1.x));
+	bool bIsOverlapY = ((minPos1.y <= maxPos0.y) && (minPos0.y <= maxPos1.y));
+
+	return bIsOverlapX && bIsOverlapY;
+}
+
 bool Rect2D::Intersect(const ICollider2D* collider) const
 {
-	return false;
+	CHECK(collider != nullptr);
+
+	EType type = collider->GetType();
+	bool bIsIntersect = false;
+
+	switch (type)
+	{
+	case ICollider2D::EType::LINE:
+	{
+		const Line2D* other = reinterpret_cast<const Line2D*>(collider);
+		bIsIntersect = other->Intersect(this);
+	}
+	break;
+
+	case ICollider2D::EType::CIRCLE:
+	{
+		const Circle2D* other = reinterpret_cast<const Circle2D*>(collider);
+		bIsIntersect = other->Intersect(this);
+	}
+	break;
+
+	case ICollider2D::EType::AABB:
+	{
+		const Rect2D* other = reinterpret_cast<const Rect2D*>(collider);
+		bIsIntersect = IsCollision(this, other);
+	}
+	break;
+
+	case ICollider2D::EType::OBB:
+	{
+		const OrientedRect2D* other = reinterpret_cast<const OrientedRect2D*>(collider);
+	}
+	break;
+
+	default: // ICollider2D::EType::NONE:
+		break;
+	}
+
+	return bIsIntersect;
 }
 
 void Rect2D::GetInterval(const glm::vec2& axis, float& outMin, float& outMax) const
