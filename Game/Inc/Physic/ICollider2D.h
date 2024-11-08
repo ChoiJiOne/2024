a1b8fb53ae1d63ff5b8c9968fcadd31c6c0e7b2b
choijione <choijione02@gmail.com>
2024-11-08 12:34:26 +0900

    2D 물리 충돌 처리를 위한 인터페이스 추가

---
+++ Game/Inc/Physic/ICollider2D.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+/** 2D 물리 충돌 처리를 위한 인터페이스입니다. */
+struct ICollider2D
+{
+	/** 2D 물리 충돌 처리 객체의 종류입니다. */
+	enum class EType
+	{
+		NONE   = 0x00,
+		POINT  = 0x01,
+		LINE   = 0x02,
+		CIRCLE = 0x03,
+		AABB   = 0x04,
+		OBB    = 0x05,
+	};
+
+	/** 2D 물리 충돌 처리를 위한 인터페이스의 기본 생성자와 빈 가상 소멸자입니다. */
+	ICollider2D() = default;
+	virtual ~ICollider2D() {}
+
+	/** 2D 물리 충돌 처리 객체의 타입을 얻습니다. */
+	virtual EType GetType() const = 0;
+	
+	/** 2D 물리 충돌 처리 객체간의 충돌 여부를 얻습니다. */
+	virtual bool Intersect(const ICollider2D* collider) const = 0;
+};
