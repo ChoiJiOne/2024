7c46ae0f2b692606c533d7124dd92c7abff7bc17
choijione <choijione02@gmail.com>
2024-11-08 09:55:28 +0900

    파일의 유효성을 검사하는 기능 추가

--- Script/TrimTextureAtlas.py
+++ Script/TrimTextureAtlas.py
@@ -3,7 +3,13 @@ import sys
 import os
 
 if __name__ == '__main__':
+    # 명령행 인수 검사.
     if len(sys.argv) != 2:
         print("\n>> Invalid command line argument! <<")
         print("Command syntax: {0} <filename>\n".format(sys.argv[0]))
         sys.exit(0)
+
+    # 파일 유효성 검사.
+    target_file_path = sys.argv[1]
+    if not os.path.exists(target_file_path):
+        print("\n>> No file found at '{0}'\n".format(target_file_path))
