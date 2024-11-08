e7d7da3507b0236e0092691a5fdf1423afc128b4
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2024-11-08 16:18:28 +0900

    2D 충돌 처리를 위한 AABB 추가

---
+++ Game/Src/Physic/Rect2D.cpp
@@ -0,0 +1,10 @@
+#include "Physic/Rect2D.h"
+
+bool Rect2D::Intersect(const ICollider2D* collider) const
+{
+	return false;
+}
+
+void Rect2D::GetInterval(const glm::vec2& axis, float& outMin, float& outMax) const
+{
+}
