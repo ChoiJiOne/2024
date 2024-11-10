ae861edcda0019cb4a9dc7bc4af8eddf8efb9f17
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2024-11-10 12:19:53 +0900

    프레임 버퍼와 포스트 프로세싱 이외의 테스트 코드 삭제

--- Game/Src/Scene/GameDevScene.cpp
+++ Game/Src/Scene/GameDevScene.cpp
@@ -13,6 +13,7 @@
 #include "GL/SpriteAnimator2D.h"
 #include "GL/Texture2D.h"
 #include "GL/TextureAtlas2D.h"
+#include "GL/PostProcessor.h"
 #include "GL/TTFont.h"
 #include "GL/VertexBuffer.h"
 #include "Scene/GameDevScene.h"
@@ -24,20 +25,19 @@ GameDevScene::GameDevScene()
 	mainCamera_ = EntityManager::GetRef().Create<Camera2D>(glm::vec2(0.0f, 0.0f), glm::vec2(1000.0f, 800.0f));
 	frameBuffer_ = GLManager::GetRef().Create<FrameBuffer>(1000, 800, FrameBuffer::EPixelFormat::RGBA, ITexture::EFilter::NEAREST);
 
-	alagard_ = GLManager::GetRef().Create<TTFont>("Resource\\alagard.ttf", 0x00, 0x128, 48.0f, ITexture::EFilter::NEAREST);
-	lower_ = GLManager::GetRef().Create<TTFont>("Resource\\lower.ttf", 0x00, 0x128, 48.0f, ITexture::EFilter::NEAREST);
-	namsan_ = GLManager::GetRef().Create<TTFont>("Resource\\namsan.ttf", 0x00, 0x128, 48.0f, ITexture::EFilter::LINEAR);
+	std::vector<uint8_t> vsBuffer;
+	std::vector<uint8_t> fsBuffer;
+	std::string vsSource;
+	std::string fsSource;
+	std::string outErrMsg;
 
-	atlas_ = GLManager::GetRef().Create<TextureAtlas2D>("Resource\\texture.png", "Resource\\texture.atlas", Texture2D::EFilter::NEAREST);
+	ASSERTION(ReadFile("Shader\\Blit.vert", vsBuffer, outErrMsg), "%s", outErrMsg.c_str());
+	ASSERTION(ReadFile("Shader\\Blit.frag", fsBuffer, outErrMsg), "%s", outErrMsg.c_str());
 
-	std::vector<std::string> names =
-	{
-		"idle-1",
-		"idle-2",
-		"idle-3",
-		"idle-4",
-	};
-	animator_ = GLManager::GetRef().Create<SpriteAnimator2D>(atlas_, names, 0.3f, true);
+	vsSource = std::string(vsBuffer.begin(), vsBuffer.end());
+	fsSource = std::string(fsBuffer.begin(), fsBuffer.end());
+
+	postProcessor_ = GLManager::GetRef().Create<PostProcessor>(vsSource, fsSource);
 }
 
 GameDevScene::~GameDevScene()
@@ -47,27 +47,13 @@ GameDevScene::~GameDevScene()
 
 void GameDevScene::Tick(float deltaSeconds)
 {
-	ImGui::Begin("OPTION");
-	ImGui::ColorEdit3("Blend", glm::value_ptr(option_.blend));
-	ImGui::ColorEdit4("Outline", glm::value_ptr(outline_));
-	ImGui::SliderFloat("Factor", &option_.factor, 0.0f, 1.0f);
-	ImGui::SliderFloat("Transparent", &option_.transparent, 0.0f, 1.0f);
-	ImGui::Checkbox("bIsFlipH", &option_.bIsFlipH);
-	ImGui::Checkbox("bIsFlipV", &option_.bIsFlipV);
-	ImGui::End();
-
-	ImVec2 mousePos = ImGui::GetMousePos();
-	mousePos_.x = -1000.0f * 0.5f + mousePos.x;
-	mousePos_.y = +800.0f * 0.5f - mousePos.y;
-
-	animator_->Update(deltaSeconds);
 }
 
 void GameDevScene::Render()
 {
 	RenderManager2D& renderMgr = RenderManager2D::GetRef();
 
-	GLManager::GetRef().BeginFrame(0.1f, 0.1f, 0.1f, 1.0f);
+	GLManager::GetRef().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
 	{
 		renderMgr.Begin(mainCamera_, frameBuffer_);
 		{
@@ -82,21 +68,10 @@ void GameDevScene::Render()
 				glm::vec4 color = (y == 0.0f) ? glm::vec4(1.0f, 0.0f, 0.0f, 1.0f) : glm::vec4(1.0f, 1.0f, 1.0f, 0.1f);
 				renderMgr.DrawLine(glm::vec2(-500.0f, y), glm::vec2(500.0f, y), color);
 			}
-
-			renderMgr.DrawRectWireframe(glm::vec2(), 200.0f, 190.0f, glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), 0.0f);
-			renderMgr.DrawTextureAtlas(animator_->GetTextureAtlas(), animator_->GetCurrentClipName(), glm::vec2(), 200.0f, 190.0f, 0.0f, glm::vec4(outline_, 1.0f), option_);
-
-			renderMgr.DrawString(namsan_, L"ABCDgyujq", glm::vec2(0.0f, 100.0f), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), glm::vec4(1.0f, 0.0f, 0.0f, 1.0f));
 		}
 		renderMgr.End();
 
-		renderMgr.Begin(mainCamera_);
-		{
-			TextureDrawOption option;
-			option.bIsFlipV = true;
-			renderMgr.DrawTexture(frameBuffer_, glm::vec2(0.0f, 0.0f), 1000.0f, 800.0f, 0.0f, option);
-		}
-		renderMgr.End();
+		postProcessor_->Blit(frameBuffer_);
 	}
 	GLManager::GetRef().EndFrame();
 }
