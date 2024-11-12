b63937d76274785447dac08e061ffec934f96049
choijione <choijione02@gmail.com>
2024-11-12 16:59:58 +0900

    게임 앱의 위치 변경 사항 적용

--- Game/Src/Main.cpp
+++ Game/Src/Main.cpp
@@ -5,7 +5,7 @@
 #include <crtdbg.h>
 #endif
 
-#include "GameApp.h"
+#include "App/GameApp.h"
 
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
