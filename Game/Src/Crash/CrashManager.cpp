e0c1beb54667bf1fd7081dabff406408afbc8486
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2024-11-12 15:46:12 +0900

    크래시 매니저 초기화 시 크래시 덤프 파일의 생성 경로 설정

--- Game/Src/Crash/CrashManager.cpp
+++ Game/Src/Crash/CrashManager.cpp
@@ -6,7 +6,15 @@
 #include "Crash/CrashManager.h"
 #include "Utils/Utils.h"
 
+static LPTOP_LEVEL_EXCEPTION_FILTER gTopLevelExceptionFilter;
+static std::wstring gCrashDumpPath;
+
 CrashManager CrashManager::instance_;
+
+LONG WINAPI DetectApplicationCrash(EXCEPTION_POINTERS* ep)
+{
+	return EXCEPTION_EXECUTE_HANDLER;
+}
 
 CrashManager& CrashManager::GetRef()
 {
@@ -20,8 +28,17 @@ CrashManager* CrashManager::GetPtr()
 
 void CrashManager::Startup()
 {
+	static wchar_t exePath[MAX_PATH];
+	GetModuleFileNameW(nullptr, exePath, MAX_PATH);
+
+	std::wstring minidumpPath(exePath);
+	std::wstring basePath = GetBasePath(minidumpPath);
+	gCrashDumpPath = PrintF(L"%s\\Crash\\", basePath.c_str());
+
+	gTopLevelExceptionFilter = ::SetUnhandledExceptionFilter(DetectApplicationCrash);
 }
 
 void CrashManager::Shutdown()
 {
+	::SetUnhandledExceptionFilter(gTopLevelExceptionFilter);
 }
