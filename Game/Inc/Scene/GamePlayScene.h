8858fffa17c756adc7a9eb200f1b1f831d0e66b8
choijione <choijione02@gmail.com>
2024-11-13 09:52:32 +0900

    게임 플레이 씬 추가

---
+++ Game/Inc/Scene/GamePlayScene.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Scene/IGameScene.h"
+
+/** 게임 플레이 씬입니다. */
+class GamePlayScene : public IGameScene
+{
+public:
+	GamePlayScene();
+	virtual ~GamePlayScene();
+
+	DISALLOW_COPY_AND_ASSIGN(GamePlayScene);
+
+	virtual void Tick(float deltaSeconds) override;
+	virtual void Render() override;
+	virtual void Enter() override;
+	virtual void Exit() override;
+
+private:
+	class Camera2D* mainCamera_ = nullptr;
+};
---
+++ Game/Src/Scene/GamePlayScene.cpp
@@ -0,0 +1,57 @@
+#include "Entity/Camera2D.h"
+#include "Entity/EntityManager.h"
+#include "GL/RenderManager2D.h"
+#include "GLFW/GLFWManager.h"
+#include "Scene/GamePlayScene.h"
+#include "Utils/Assertion.h"
+#include "Utils/Utils.h"
+
+GamePlayScene::GamePlayScene()
+{
+	glm::vec2 screenSize = glm::vec2(0.0f, 0.0f);
+	GLFWManager::GetRef().GetWindowSize(screenSize.x, screenSize.y);
+
+	mainCamera_ = EntityManager::GetRef().Create<Camera2D>(glm::vec2(0.0f, 0.0f), screenSize);
+}
+
+GamePlayScene::~GamePlayScene()
+{
+	EntityManager::GetRef().Destroy(mainCamera_);
+}
+
+void GamePlayScene::Tick(float deltaSeconds)
+{
+}
+
+void GamePlayScene::Render()
+{
+	RenderManager2D& renderMgr = RenderManager2D::GetRef();
+
+	GLManager::GetRef().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
+	{
+		renderMgr.Begin(mainCamera_);
+		{
+			for (float x = -500; x <= 500.0f; x += 10.0f)
+			{
+				glm::vec4 color = (x == 0.0f) ? glm::vec4(0.0f, 0.0f, 1.0f, 1.0f) : glm::vec4(1.0f, 1.0f, 1.0f, 0.1f);
+				renderMgr.DrawLine(glm::vec2(x, 400.0f), glm::vec2(x, -400.0f), color);
+			}
+
+			for (float y = -400; y <= 400.0f; y += 10.0f)
+			{
+				glm::vec4 color = (y == 0.0f) ? glm::vec4(1.0f, 0.0f, 0.0f, 1.0f) : glm::vec4(1.0f, 1.0f, 1.0f, 0.1f);
+				renderMgr.DrawLine(glm::vec2(-500.0f, y), glm::vec2(500.0f, y), color);
+			}
+		}
+		renderMgr.End();
+	}
+	GLManager::GetRef().EndFrame();
+}
+
+void GamePlayScene::Enter()
+{
+}
+
+void GamePlayScene::Exit()
+{
+}
