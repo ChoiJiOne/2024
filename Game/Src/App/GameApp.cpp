7ac1b8f947030fdff530d1da6eca443c2341087f
choijione <choijione02@gmail.com>
2024-11-13 11:58:46 +0900

    게임 앱에서 텍스처 아틀라스 리소스 생성

--- Game/Src/App/GameApp.cpp
+++ Game/Src/App/GameApp.cpp
@@ -1,4 +1,6 @@
 #include "App/GameApp.h"
+#include "GL/GLManager.h"
+#include "GL/TextureAtlas2D.h"
 #include "Scene/GamePlayScene.h"
 #include "Scene/SceneManager.h"
 
@@ -13,6 +15,9 @@ GameApp::~GameApp()
 
 void GameApp::Startup()
 {
+	TextureAtlas2D* textureAtlas = glManager_->Create<TextureAtlas2D>("Resource\\TextureAtlas\\TextureAtlas.png", "Resource\\TextureAtlas\\TextureAtlas.atlas", TextureAtlas2D::EFilter::NEAREST);
+	glManager_->Register("TextureAtlas", textureAtlas);
+
 	GamePlayScene* gamePlayScene = sceneManager_->Create<GamePlayScene>();
 	sceneManager_->Register("GamePlayScene", gamePlayScene);
 
