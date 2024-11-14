21a1c2187d52e6abd64711041ed480a6549412b4
choijione <choijione02@gmail.com>
2024-11-14 20:01:59 +0900

    플레이어의 애니메이션 로딩 및 업데이트기능 추가
    - ImGui를 이용해서 애니메이션 컨트롤 테스트를 수행하는 테스트 코드 추가

--- Game/Inc/Entity/Player.h
+++ Game/Inc/Entity/Player.h
@@ -1,8 +1,14 @@
 #pragma once
 
+#include <map>
+
 #include "Entity/IEntity2D.h"
 #include "Physic/Circle2D.h"
 #include "Physic/Rect2D.h"
+
+/** 클래스 전방 선언입니다. */
+class SpriteAnimator2D;
+class TextureAtlas2D;
 
 /** 플레이어가 조종할 엔티티입니다. */
 class Player : public IEntity2D
@@ -18,12 +24,33 @@ public:
 	virtual void Release() override;
 
 private:
+	/** 플레이어의 애니메이션 상태입니다. */
+	enum class EAnimationState
+	{
+		IDLE = 0x00,
+		RUN  = 0x01,
+		HURT = 0x02,
+	};
+
+	/** 플레이어의 애니메이션 상태를 초기화합니다. */
+	void LoadAnimations();
+
+	/** 플레이어의 애니메이션 상태 초기화를 해제합니다. */
+	void UnloadAnimation();
+
+private:
 	/** 플레이어 렌더링 시 참조할 텍스처 아틀라스입니다. */
-	class TextureAtlas2D* textureAtlas_ = nullptr;
+	TextureAtlas2D* textureAtlas_ = nullptr;
 
 	/** 렌더링 영역입니다 */
 	Rect2D renderBound_;
 
 	/** 충돌 영역입니다. */
 	Circle2D collisionBound_;
+
+	/** 현재 애니메이션의 상태입니다. */
+	EAnimationState animationState_ = EAnimationState::IDLE;
+
+	/** 플레이어의 애니메이션 상태 별 애니메이션 리소스입니다. */
+	std::map<EAnimationState, SpriteAnimator2D*> animations_;
 };
--- Game/Src/Entity/Player.cpp
+++ Game/Src/Entity/Player.cpp
@@ -3,6 +3,7 @@
 #include "Entity/Player.h"
 #include "GL/GLManager.h"
 #include "GL/RenderManager2D.h"
+#include "GL/SpriteAnimator2D.h"
 #include "GL/Texture2D.h"
 #include "GLFW/GLFWManager.h"
 #include "Utils/Assertion.h"
@@ -14,6 +15,8 @@ Player::Player()
 	collisionBound_.radius = 22.0f;
 	collisionBound_.center.x = 0.0f;
 	collisionBound_.center.y = renderBound_.center.y - renderBound_.size.y * 0.5f + collisionBound_.radius;
+
+	LoadAnimations();
 
 	bIsInitialized_ = true;
 }
@@ -28,18 +31,37 @@ Player::~Player()
 
 void Player::Tick(float deltaSeconds)
 {
-	ImGui::Begin("CONTROL");
-	ImGui::SliderFloat("Radius", &collisionBound_.radius, 0.0f, 30.0f);
+	ImGui::Begin("ANIMATION");
+	if (ImGui::RadioButton("IDLE", static_cast<int32_t>(animationState_) == 0))
+	{
+		animationState_ = EAnimationState::IDLE;
+	}
 
-	collisionBound_.center.y = renderBound_.center.y - renderBound_.size.y * 0.5f + collisionBound_.radius;
+	if (ImGui::RadioButton("RUN", static_cast<int32_t>(animationState_) == 1))
+	{
+		animationState_ = EAnimationState::RUN;
+	}
 
+	if (ImGui::RadioButton("HURT", static_cast<int32_t>(animationState_) == 2))
+	{
+		animationState_ = EAnimationState::HURT;
+	}
 
 	ImGui::End();
+
+	animations_.at(animationState_)->Update(deltaSeconds);
 }
 
 void Player::Render()
 {
-	RenderManager2D::GetRef().DrawTextureAtlas(textureAtlas_, "Foxy_Idle_1", renderBound_.center, renderBound_.size.x, renderBound_.size.y, 0.0f);
+	RenderManager2D::GetRef().DrawTextureAtlas(
+		animations_.at(animationState_)->GetTextureAtlas(),
+		animations_.at(animationState_)->GetCurrentClipName(),
+		renderBound_.center, 
+		renderBound_.size.x, 
+		renderBound_.size.y, 
+		0.0f
+	);
 	RenderManager2D::GetRef().DrawRectWireframe(renderBound_.center, renderBound_.size.x, renderBound_.size.y, glm::vec4(1.0f, 0.0f, 0.0f, 1.0f), 0.0f);
 	RenderManager2D::GetRef().DrawLine(glm::vec2(-500.0f, 0.0f), glm::vec2(500.0f, 0.0f), glm::vec4(1.0f, 0.0f, 0.0f, 1.0f));
 	RenderManager2D::GetRef().DrawLine(glm::vec2(0.0f, 400.0f), glm::vec2(0.0f, -400.0f), glm::vec4(1.0f, 0.0f, 0.0f, 1.0f));
@@ -50,7 +72,52 @@ void Player::Release()
 {
 	CHECK(bIsInitialized_);
 
+	UnloadAnimation();
 	textureAtlas_ = nullptr;
 
 	bIsInitialized_ = false;
+}
+
+void Player::LoadAnimations()
+{
+	GLManager& glManager = GLManager::GetRef();
+
+	std::vector<std::string> idleClipNames =
+	{
+		"Foxy_Idle_1",
+		"Foxy_Idle_2",
+		"Foxy_Idle_3",
+		"Foxy_Idle_4",
+	};
+	SpriteAnimator2D* idleAnimation = glManager.Create<SpriteAnimator2D>(textureAtlas_, idleClipNames, 0.5f, true);
+	animations_.insert( { EAnimationState::IDLE, idleAnimation } );
+
+	std::vector<std::string> runClipNames =
+	{
+		"Foxy_Run_1",
+		"Foxy_Run_2",
+		"Foxy_Run_3",
+		"Foxy_Run_4",
+		"Foxy_Run_5",
+		"Foxy_Run_6",
+	};
+	SpriteAnimator2D* runAnimation = glManager.Create<SpriteAnimator2D>(textureAtlas_, runClipNames, 0.5f, true);
+	animations_.insert({ EAnimationState::RUN, runAnimation });
+
+	std::vector<std::string> hurtClipNames =
+	{
+		"Foxy_Hurt_1",
+		"Foxy_Hurt_2",
+	};
+	SpriteAnimator2D* hurtAnimation = glManager.Create<SpriteAnimator2D>(textureAtlas_, hurtClipNames, 0.3f, true);
+	animations_.insert({ EAnimationState::HURT, hurtAnimation });
+}
+
+void Player::UnloadAnimation()
+{
+	GLManager& glManager = GLManager::GetRef();
+	for (auto& animation : animations_)
+	{
+		glManager.Destroy(animation.second);
+	}
 }
