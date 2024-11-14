90d6f78c33352c9889a395d63cf4c9ce4422168d
choijione <choijione02@gmail.com>
2024-11-15 07:23:47 +0900

    2D 엔티티의 렌더링 순서 값에 대한 Getter/Setter 추가

--- Game/Inc/Entity/IEntity2D.h
+++ Game/Inc/Entity/IEntity2D.h
@@ -14,4 +14,14 @@ public:
 	virtual void Tick(float deltaSeconds) = 0;
 	virtual void Render() = 0;
 	virtual void Release() = 0;
+
+	/** 2D 엔티티의 렌더링 순서 값을 설정합니다. */
+	void SetRenderOrder(uint32_t renderOrder) { renderOrder_ = renderOrder; }
+
+	/** 2D 엔티티의 렌더링 순서 값을 얻습니다. */
+	uint32_t GetRenderOrder() const { return renderOrder_; }
+
+private:
+	/** 2D 엔티티의 렌더링 순위 값입니다. 이 값은 낮을수록 우선순위가 높아집니다. */
+	uint32_t renderOrder_ = 0;
 };
