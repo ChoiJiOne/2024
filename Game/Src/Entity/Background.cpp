15a4009c32e463f207796be2c834938ade58ca47
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2024-11-16 10:34:07 +0900

    백그라운드 엔티티가 플레이어를 따라다니도록 수정

--- Game/Src/Entity/Background.cpp
+++ Game/Src/Entity/Background.cpp
@@ -1,4 +1,6 @@
 #include "Entity/Background.h"
+#include "Entity/EntityManager.h"
+#include "Entity/Player.h"
 #include "GL/GLManager.h"
 #include "GL/RenderManager2D.h"
 #include "GL/Texture2D.h"
@@ -10,11 +12,13 @@ Background::Background()
 	tickOrder_ = 2;
 	renderOrder_ = 0;
 
+	player_ = EntityManager::GetRef().GetByName<Player>("Player");
+
 	glm::vec2 screenSize = glm::vec2(0.0f, 0.0f);
 	GLFWManager::GetRef().GetWindowSize(screenSize.x, screenSize.y);
 
 	textureAtlas_ = GLManager::GetRef().GetByName<TextureAtlas2D>("TextureAtlas");
-	bound_ = Rect2D(glm::vec2(0.0f, 0.0f), screenSize);
+	bound_ = Rect2D(player_->GetPosition(), screenSize);
 
 	bIsInitialized_ = true;
 }
@@ -29,6 +33,7 @@ Background::~Background()
 
 void Background::Tick(float deltaSeconds)
 {
+	bound_.center = player_->GetPosition();
 }
 
 void Background::Render()
@@ -41,6 +46,7 @@ void Background::Release()
 	CHECK(bIsInitialized_);
 
 	textureAtlas_ = nullptr;
+	player_ = nullptr;
 
 	bIsInitialized_ = false;
 }
