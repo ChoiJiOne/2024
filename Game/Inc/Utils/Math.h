520fd18759a3bb563c9fd4a8aef14eb38b3fc2ad
choijione <choijione02@gmail.com>
2024-11-18 06:53:32 +0900

    특정 범위 내의 난수를 생성하는 기능 추가

--- Game/Inc/Utils/Math.h
+++ Game/Inc/Utils/Math.h
@@ -7,3 +7,9 @@
  * 이때, 각도의 범위는 [0, 2pi] 입니다.
  */
 float GetRadianVec2(const glm::vec2& v);
+
+/** [minValue, maxValue] 범위의 임의의 난수를 생성합니다. */
+int32_t GenerateRandomInt(int32_t minValue, int32_t maxValue);
+
+/** [minValue, maxValue] 범위의 임의의 부동 소수점 난수를 생성합니다. */
+float GenerateRandomFloat(float minValue, float maxValue);
--- Game/Src/Utils/Math.cpp
+++ Game/Src/Utils/Math.cpp
@@ -1,3 +1,6 @@
+#include <chrono>
+#include <random>
+
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/vector_angle.hpp>
 
@@ -7,4 +10,22 @@ float GetRadianVec2(const glm::vec2& v)
 {
 	float rotate = glm::angle(glm::vec2(1.0f, 0.0f), v);
 	return v.y < 0.0f ? (2.0f * glm::pi<float>() - rotate) : rotate;
+}
+
+int32_t GenerateRandomInt(int32_t minValue, int32_t maxValue)
+{
+	std::random_device randomDevice;
+	std::mt19937 generator(randomDevice());
+	std::uniform_int_distribution<int32_t> distribution(glm::min<int32_t>(minValue, maxValue), glm::max<int32_t>(minValue, maxValue));
+
+	return distribution(generator);
+}
+
+float GenerateRandomFloat(float minValue, float maxValue)
+{
+	std::random_device randomDevice;
+	std::mt19937 generator(randomDevice());
+	std::uniform_real_distribution<float> distribution(glm::min<float>(minValue, maxValue), glm::max<float>(minValue, maxValue));
+
+	return distribution(generator);
 }
