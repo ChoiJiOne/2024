075e8c566848c7ff59fa781d444b340593015339
choijione <choijione02@gmail.com>
2024-11-17 12:20:06 +0900

    플레이어가 피해야 할 불 엔티티 추가

---
+++ Game/Inc/Entity/Fire.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <map>
+
+#include "Entity/IEntity2D.h"
+#include "GL/RenderManager2D.h"
+#include "Physic/Circle2D.h"
+#include "Physic/OrientedRect2D.h"
+
+/** 클래스 전방 선언입니다. */
+class SpriteAnimator2D;
+class TextureAtlas2D;
+class Player;
+class Playground;
+
+/** 플레이어가 피해야 할 불입니다. */
+class Fire : public IEntity2D
+{
+public:
+	/** 불의 상태입니다. */
+	enum class EState
+	{
+		NONE      = 0x00, // 아무것도 아닌 상태.
+		MOVE      = 0x01, // 움직이고 있는 상태.
+		EXPLOSION = 0x02, // 불이 폭발하는 상태.
+		DONE      = 0x03, // 더 이상 업데이트할 수 없는 상태.
+	};
+
+public:
+	Fire(const glm::vec2& position, const glm::vec2& direction, float speed);
+	virtual ~Fire();
+
+	DISALLOW_COPY_AND_ASSIGN(Fire);
+
+	virtual void Tick(float deltaSeconds) override;
+	virtual void Render() override;
+	virtual void Release() override;
+
+	/** 불의 현재 상태를 얻습니다. */
+	const EState& GetState() const { return state_; }
+
+private:
+	/** 불을 움직입니다. */
+	void Move(float deltaSeconds);
+
+private:
+	/** 코인 렌더링 시 참조할 텍스처 아틀라스입니다. */
+	TextureAtlas2D* textureAtlas_ = nullptr;
+	
+	/** 해당 코인을 획득했는지 확인할 플레이어입니다. */
+	Player* player_ = nullptr;
+
+	/** 불이 존재할 영역입니다. */
+	Playground* playground_ = nullptr;
+
+	/** 렌더링 영역입니다. */
+	OrientedRect2D renderBound_;
+
+	/** 충돌 영역입니다. */
+	Circle2D collisionBound_;
+
+	/** 불의 이동 방향입니다. */
+	glm::vec2 direction_ = glm::vec2(0.0f, 0.0f);
+
+	/** 불의 이동 속도입니다. */
+	float speed_ = 0.0f;
+
+	/** 불의 현재 상태입니다. */
+	EState state_ = EState::NONE;
+
+	/** 불의 상태 별 애니메이션입니다. */
+	std::map<EState, SpriteAnimator2D*> animations_;
+};
---
+++ Game/Src/Entity/Fire.cpp
@@ -0,0 +1,146 @@
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/vector_angle.hpp>
+
+#include "Entity/Fire.h"
+#include "Entity/EntityManager.h"
+#include "Entity/Player.h"
+#include "Entity/Playground.h"
+#include "GL/GLManager.h"
+#include "GL/SpriteAnimator2D.h"
+#include "GL/TextureAtlas2D.h"
+#include "Utils/Assertion.h"
+
+Fire::Fire(const glm::vec2& position, const glm::vec2& direction, float speed)
+	: direction_(direction)
+	, speed_(speed)
+{
+	tickOrder_ = 3;
+	renderOrder_ = 4;
+
+	textureAtlas_ = GLManager::GetRef().GetByName<TextureAtlas2D>("TextureAtlas");
+	player_ = EntityManager::GetRef().GetByName<Player>("Player");
+	playground_ = EntityManager::GetRef().GetByName<Playground>("Playground");
+	
+	float rotate = glm::angle(glm::vec2(1.0f, 0.0f), direction_);
+	rotate = direction_.y < 0.0f ? (2.0f * glm::pi<float>() - rotate) : rotate;
+	renderBound_ = OrientedRect2D(position, glm::vec2(58.0f, 54.0f), rotate);
+
+	collisionBound_.center = renderBound_.center;
+	collisionBound_.radius = 20.0f;
+
+	std::vector<std::string> fireBallClipNames =
+	{
+		"Fireball_1",
+		"Fireball_2",
+		"Fireball_3",
+		"Fireball_4",
+		"Fireball_5",
+	};
+	SpriteAnimator2D* fireBall = GLManager::GetRef().Create<SpriteAnimator2D>(textureAtlas_, fireBallClipNames, 0.2f, true);
+
+	std::vector<std::string> fireBallHitClipNames =
+	{
+		"Fireball_Hit_1",
+		"Fireball_Hit_2",
+		"Fireball_Hit_3",
+		"Fireball_Hit_4",
+		"Fireball_Hit_5",
+		"Fireball_Hit_6",
+	};
+	SpriteAnimator2D* fireBallHit = GLManager::GetRef().Create<SpriteAnimator2D>(textureAtlas_, fireBallHitClipNames, 0.5f, false);
+
+	animations_ =
+	{
+		{ EState::NONE,       nullptr     },
+		{ EState::MOVE,       fireBall    },
+		{ EState::EXPLOSION,  fireBallHit },
+		{ EState::DONE,       nullptr     },
+	};
+
+	state_ = EState::MOVE;
+
+	bIsInitialized_ = true;
+}
+
+Fire::~Fire()
+{
+	if (bIsInitialized_)
+	{
+		Release();
+	}
+}
+
+void Fire::Tick(float deltaSeconds)
+{
+	switch (state_)
+	{
+	case EState::MOVE:
+	{
+		Move(deltaSeconds);
+		animations_.at(state_)->Update(deltaSeconds);
+
+		if (collisionBound_.Intersect(player_->GetCollider()))
+		{
+			state_ = EState::EXPLOSION;
+		}
+
+		if (!collisionBound_.Intersect(playground_->GetSafeBound()))
+		{
+			state_ = EState::EXPLOSION;
+		}
+	}
+	break;
+
+	case EState::EXPLOSION:
+	{
+		SpriteAnimator2D* animation = animations_.at(state_);
+		animation->Update(deltaSeconds);
+		if (animation->GetTime() >= animation->GetAnimationTime())
+		{
+			state_ = EState::DONE;
+		}
+	}
+	break;
+	
+	case EState::DONE:
+	{
+
+	}
+	break;
+	}
+}
+
+void Fire::Render()
+{
+	if (state_ == EState::NONE || state_ == EState::DONE)
+	{
+		return;
+	}
+
+	TextureAtlas2D* animationTexture = animations_.at(state_)->GetTextureAtlas();
+	const std::string& animationClipName = animations_.at(state_)->GetCurrentClipName();
+
+	renderManager_->DrawTextureAtlas(animationTexture, animationClipName, renderBound_.center, renderBound_.size.x, renderBound_.size.y, renderBound_.rotate);
+}
+
+void Fire::Release()
+{
+	CHECK(bIsInitialized_);
+
+	GLManager::GetRef().Destroy(animations_.at(EState::MOVE));
+	GLManager::GetRef().Destroy(animations_.at(EState::EXPLOSION));
+
+	playground_ = nullptr;
+	player_ = nullptr;
+	textureAtlas_ = nullptr;
+	
+	bIsInitialized_ = false;
+}
+
+void Fire::Move(float deltaSeconds)
+{
+	glm::vec2 movePos = renderBound_.center + direction_ * deltaSeconds * speed_;
+
+	renderBound_.center = movePos;
+	collisionBound_.center = movePos;
+}
