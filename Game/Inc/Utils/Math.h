a8a1281af98609977ccc71b999c318ce7d63b175
choijione <choijione02@gmail.com>
2024-11-18 06:47:32 +0900

    2차원 벡터의 라디안 각도 값을 얻는 기능 추가

--- Game/Inc/Utils/Math.h
+++ Game/Inc/Utils/Math.h
@@ -2,3 +2,8 @@
 
 #include <glm/glm.hpp>
 
+/**
+ * 2차원 벡터의 라디안 각도 값을 얻습니다.
+ * 이때, 각도의 범위는 [0, 2pi] 입니다.
+ */
+float GetRadianVec2(const glm::vec2& v);
--- Game/Src/Utils/Math.cpp
+++ Game/Src/Utils/Math.cpp
@@ -1,1 +1,10 @@
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/vector_angle.hpp>
+
 #include "Utils/Math.h"
+
+float GetRadianVec2(const glm::vec2& v)
+{
+	float rotate = glm::angle(glm::vec2(1.0f, 0.0f), v);
+	return v.y < 0.0f ? (2.0f * glm::pi<float>() - rotate) : rotate;
+}
