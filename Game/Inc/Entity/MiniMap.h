8b44066ba9fd138aba591049979594336ac59295
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2024-11-18 17:04:56 +0900

    아틀라스 내에 미니맵 텍스처를 렌더링하는 기능 추가

--- Game/Inc/Entity/MiniMap.h
+++ Game/Inc/Entity/MiniMap.h
@@ -2,19 +2,20 @@
 
 #include "Entity/IEntity2D.h"
 #include "GL/RenderManager2D.h"
-#include "Physic/Circle2D.h"
+#include "Physic/Rect2D.h"
 
 /** 클래스 전방 선언입니다. */
 class UICamera;
 class RandomChest;
 class Player;
+class Playground;
 class TextureAtlas2D;
 
 /** 전체 게임 월드를 축소해서 보여주는 미니맵 엔티티입니다. */
 class MiniMap : public IEntity2D
 {
 public:
-	MiniMap();
+	MiniMap(UICamera* uiCamera);
 	virtual ~MiniMap();
 
 	DISALLOW_COPY_AND_ASSIGN(MiniMap);
@@ -24,5 +25,12 @@ public:
 	virtual void Release() override;
 
 private:
+	/** 미니멥 렌더링에 기준이 될 UI 카메라입니다. */
+	UICamera* uiCamera_ = nullptr;
 
+	/** 미니맵 렌더링 시 참조할 텍스처 아틀라스입니다. */
+	TextureAtlas2D* textureAtlas_ = nullptr;
+
+	/** 렌더링 영역입니다. */
+	Rect2D renderBound_;
 };
--- Game/Src/Entity/MiniMap.cpp
+++ Game/Src/Entity/MiniMap.cpp
@@ -1,15 +1,24 @@
 #include "Entity/UICamera.h"
 #include "Entity/MiniMap.h"
 #include "Entity/Player.h"
+#include "Entity/Playground.h"
 #include "GL/GLManager.h"
 #include "GL/TextureAtlas2D.h"
 #include "GLFW/GLFWManager.h"
 #include "Utils/Assertion.h"
 
-MiniMap::MiniMap()
+MiniMap::MiniMap(UICamera* uiCamera)
+	: uiCamera_(uiCamera)
 {
 	tickOrder_ = 0;
 	renderOrder_ = 0;
+
+	textureAtlas_ = GLManager::GetRef().GetByName<TextureAtlas2D>("TextureAtlas");
+	
+	glm::vec2 uiCameraSize = uiCamera_->GetSize() * 0.5f;
+	renderBound_ = Rect2D(glm::vec2(0.0f, 0.0f), glm::vec2(256.0f, 256.0f));
+	renderBound_.center = glm::vec2(uiCameraSize.x, -uiCameraSize.y);
+	renderBound_.center += glm::vec2(-renderBound_.size.x, +renderBound_.size.y) * 0.5f;
 
 	bIsInitialized_ = true;
 }
@@ -28,11 +37,14 @@ void MiniMap::Tick(float deltaSeconds)
 
 void MiniMap::Render()
 {
+	renderManager_->DrawTextureAtlas(textureAtlas_, "MiniMap", renderBound_.center, renderBound_.size.x, renderBound_.size.y, 0.0f);
 }
 
 void MiniMap::Release()
 {
 	CHECK(bIsInitialized_);
+
+	textureAtlas_ = nullptr;
 
 	bIsInitialized_ = false;
 }
