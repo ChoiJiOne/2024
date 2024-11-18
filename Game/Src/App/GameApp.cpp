5cb2140e029fd89c045c3017f82abf2c0928917a
choijione <choijione02@gmail.com>
2024-11-19 08:38:46 +0900

    게임 앱 내부에서 폰트 리소스 로딩

--- Game/Src/App/GameApp.cpp
+++ Game/Src/App/GameApp.cpp
@@ -1,8 +1,10 @@
 #include "App/GameApp.h"
 #include "GL/GLManager.h"
 #include "GL/TextureAtlas2D.h"
+#include "GL/TTFont.h"
 #include "Scene/GamePlayScene.h"
 #include "Scene/SceneManager.h"
+#include "Utils/Utils.h"
 
 GameApp::GameApp(uint32_t windowWidth, uint32_t windowHeight, const char* windowTitle, bool bIsWindowCentered)
 	: IApp(windowWidth, windowHeight, windowTitle, bIsWindowCentered)
@@ -17,6 +19,13 @@ void GameApp::Startup()
 {
 	TextureAtlas2D* textureAtlas = glManager_->Create<TextureAtlas2D>("Resource\\TextureAtlas\\TextureAtlas.png", "Resource\\TextureAtlas\\TextureAtlas.atlas", TextureAtlas2D::EFilter::NEAREST);
 	glManager_->Register("TextureAtlas", textureAtlas);
+
+	std::array<int32_t, 4> fontSizes = { 24, 32, };
+	for (const auto& fontSize : fontSizes)
+	{
+		TTFont* font = glManager_->Create<TTFont>("Resource\\Font\\lower.ttf", 0x00, 0x128, static_cast<float>(fontSize));
+		glManager_->Register(PrintF("Font%d", fontSize), font);
+	}
 
 	GamePlayScene* gamePlayScene = sceneManager_->Create<GamePlayScene>();
 
