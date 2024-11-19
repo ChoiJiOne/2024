23bcc8711bf5fa87f1f2453941a4c53e505fdb0a
choijione <choijione02@gmail.com>
2024-11-20 07:22:42 +0900

    구현 계획 변경으로 플레이 그라운드의 필요 없어진 요소들 모두 삭제

--- Game/Inc/Entity/Playground.h
+++ Game/Inc/Entity/Playground.h
@@ -5,20 +5,9 @@
 #include "Entity/IEntity2D.h"
 #include "Physic/Circle2D.h"
 
-class Player;
-
 /** 플레이어의 플레이 영역을 나타내는 엔티티입니다. */
 class Playground : public IEntity2D
 {
-public:
-	/** 플레이어의 현재 위치 상태를 얻습니다. */
-	enum class EPlayerState
-	{
-		SAFE = 0x00,
-		WARN = 0x01,
-		DEAD = 0x02,
-	};
-
 public:
 	Playground();
 	virtual ~Playground();
@@ -35,9 +24,6 @@ public:
 	/** 위험한 영역을 얻습니다. */
 	Circle2D* GetWarnBound() { return &warnBound_; }
 
-	/** 플레이어 현재 위치에 대한 상태를 얻습니다. */
-	const EPlayerState& GetPlayerState() const { return playerState_; }
-	
 private:
 	/** 안전 영역입니다. */
 	Circle2D safeBound_;
@@ -45,21 +31,6 @@ private:
 	/** 위험한 영역입니다. */
 	Circle2D warnBound_;
 
-	/** 플레이어입니다. */
-	Player* player_ = nullptr;
-
-	/** 플레이어 현재 위치에 대한 상태입니다. */
-	EPlayerState playerState_ = EPlayerState::SAFE;
-
-	/** 플레이어가 위험한 영역에 있는 시간입니다. */
-	float warnStayTime_ = 0.0f;
-
-	/** 플레이어가 위험한 영역에 있는 최대 시간입니다. */
-	float maxWarnStayTime_ = 0.0f;
-
-	/** 플레이어가 위험한 영역에 있을 때 받을 데미지입니다. */
-	float deadDamage_ = 0.0f;
-
-	/** 상태 별 색상입니다. */
-	std::map<EPlayerState, glm::vec4> playerStateColors_;
+	/** 플레이 영역을 명확하게 표시하기 위한 색상입니다. */
+	glm::vec4 outlineColor_;
 };
--- Game/Src/Entity/Playground.cpp
+++ Game/Src/Entity/Playground.cpp
@@ -1,5 +1,4 @@
 #include "Entity/EntityManager.h"
-#include "Entity/Player.h"
 #include "Entity/Playground.h"
 #include "GL/RenderManager2D.h"
 #include "Utils/Assertion.h"
@@ -9,20 +8,10 @@ Playground::Playground()
 	tickOrder_ = 3;
 	renderOrder_ = 1;
 
-	player_ = EntityManager::GetRef().GetByName<Player>("Player");
-
 	safeBound_ = Circle2D(glm::vec2(0.0f, 0.0f), 1000.0f);
 	warnBound_ = Circle2D(glm::vec2(0.0f, 0.0f), 1050.0f);
-	playerStateColors_ =
-	{
-		{ EPlayerState::SAFE, glm::vec4(0.0f, 1.0f, 0.0f, 1.0f)   },
-		{ EPlayerState::WARN, glm::vec4(1.0f, 0.647f, 0.0f, 1.0f) },
-		{ EPlayerState::DEAD, glm::vec4(1.0f, 0.0f, 0.0f, 1.0f)   },
-	};
 
-	warnStayTime_ = 0.0f;
-	maxWarnStayTime_ = 3.0f;
-	deadDamage_ = 50.0f;
+	outlineColor_ = glm::vec4(1.0f, 0.2f, 0.2f, 1.0f);
 
 	bIsInitialized_ = true;
 }
@@ -37,41 +26,16 @@ Playground::~Playground()
 
 void Playground::Tick(float deltaSeconds)
 {
-	if (!safeBound_.Intersect(player_->GetCollider()))
-	{
-		warnStayTime_ += deltaSeconds;
-		if (!warnBound_.Intersect(player_->GetCollider()) && warnStayTime_ >= maxWarnStayTime_)
-		{
-			warnStayTime_ -= maxWarnStayTime_;
-
-			float hp = player_->GetHP();
-			hp -= deadDamage_;
-			player_->SetHP(hp);
-
-			playerState_ = EPlayerState::DEAD;
-		}
-		else
-		{
-			playerState_ = EPlayerState::WARN;
-		}
-	}
-	else
-	{
-		warnStayTime_ = 0.0f;
-		playerState_ = EPlayerState::SAFE;
-	}
 }
 
 void Playground::Render()
 {
-	renderManager_->DrawCircleWireframe(safeBound_.center, safeBound_.radius, playerStateColors_.at(playerState_));
+	renderManager_->DrawCircleWireframe(safeBound_.center, safeBound_.radius, outlineColor_);
 }
 
 void Playground::Release()
 {
 	CHECK(bIsInitialized_);
-
-	player_ = nullptr;
 
 	bIsInitialized_ = false;
 }
