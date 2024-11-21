631fda4a1997173e00a3672f50c7e66d340aeb00
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2024-11-21 17:18:57 +0900

    게임 플레이 씬에 메신저 엔티티 추가

--- Game/Src/Scene/GamePlayScene.cpp
+++ Game/Src/Scene/GamePlayScene.cpp
@@ -6,6 +6,7 @@
 #include "Entity/Coin.h"
 #include "Entity/CoinCollector.h"
 #include "Entity/EntityManager.h"
+#include "Entity/Messenger.h"
 #include "Entity/MiniMap.h"
 #include "Entity/Player.h"
 #include "Entity/PlayerFollowCamera.h"
@@ -27,6 +28,11 @@ GamePlayScene::GamePlayScene()
 	uiCamera_ = entityManager_->Create<UICamera>();
 	entityManager_->Register("UICamera", uiCamera_);
 	AddUpdateUIEntity(uiCamera_);
+
+	Messenger* messenger = entityManager_->Create<Messenger>(uiCamera_, glManager_->GetByName<TTFont>("Font24"));
+	entityManager_->Register("Messenger", messenger);
+	AddUpdateUIEntity(messenger);
+	AddRenderUIEntity(messenger);
 
 	Playground* playground = entityManager_->Create<Playground>();
 	entityManager_->Register("Playground", playground);
