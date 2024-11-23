0e5b9c3c7382c5baa4c289e883c4910b373c4784
choijione <choijione02@gmail.com>
2024-11-24 08:13:27 +0900

    게임 타이틀 씬의 초기화/해제 기능 추가

--- Game/Inc/Scene/GameTitleScene.h
+++ Game/Inc/Scene/GameTitleScene.h
@@ -17,12 +17,19 @@ public:
 	virtual void Exit() override;
 
 private:
+	/** 게임 타이틀 씬을 초기화합니다. */
+	void Initailize();
+
+	/** 게임 타이틀 씬의 초기화를 해제합니다. */
+	void UnInitailize();
+
+private:
 	/** 게임 타이틀 씬의 스크린 카메라입니다. */
 	class UICamera* uiCamera_ = nullptr;
-
+	
 	/** 업데이트할 UI 엔티티입니다. */
-	std::list<IEntity*> updateUiEntities_;
+	std::map<std::string, IEntity*> updateUiEntities_;
 
 	/** 렌더링할 UI 엔티티입니다. */
-	std::list<IEntity2D*> renderUiEntities_;
+	std::map<std::string, IEntity2D*> renderUiEntities_;
 };
--- Game/Src/Scene/GameTitleScene.cpp
+++ Game/Src/Scene/GameTitleScene.cpp
@@ -16,43 +16,19 @@ GameTitleScene::GameTitleScene()
 {
 	sceneManager_->Register("GameTitleScene", this);
 
-	uiCamera_ = entityManager_->GetByName<UICamera>("UICamera");
-	updateUiEntities_.push_back(uiCamera_);
-
-	Backdrop* backdrop = entityManager_->Create<Backdrop>();
-	updateUiEntities_.push_back(backdrop);
-	renderUiEntities_.push_back(backdrop);
-
-	Title* title = entityManager_->Create<Title>();
-	updateUiEntities_.push_back(title);
-	renderUiEntities_.push_back(title);
-
-	UIButton* startBtn = entityManager_->Create<UIButton>("Resource\\UI\\Start.button", uiCamera_, glManager_->GetByName<TTFont>("Font48"), EMouse::LEFT, [&]() {});
-	updateUiEntities_.push_back(startBtn);
-	renderUiEntities_.push_back(startBtn);
-
-	UIButton* galleryBtn = entityManager_->Create<UIButton>("Resource\\UI\\Gallery.button", uiCamera_, glManager_->GetByName<TTFont>("Font48"), EMouse::LEFT, [&]() {});
-	updateUiEntities_.push_back(galleryBtn);
-	renderUiEntities_.push_back(galleryBtn);
-
-	UIButton* optionBtn = entityManager_->Create<UIButton>("Resource\\UI\\Option.button", uiCamera_, glManager_->GetByName<TTFont>("Font48"), EMouse::LEFT, [&]() {});
-	updateUiEntities_.push_back(optionBtn);
-	renderUiEntities_.push_back(optionBtn);
-
-	UIButton* quitBtn = entityManager_->Create<UIButton>("Resource\\UI\\Quit.button", uiCamera_, glManager_->GetByName<TTFont>("Font48"), EMouse::LEFT, [&]() {});
-	updateUiEntities_.push_back(quitBtn);
-	renderUiEntities_.push_back(quitBtn);
+	Initailize();
 }
 
 GameTitleScene::~GameTitleScene()
 {
+	UnInitailize();
 }
 
 void GameTitleScene::Tick(float deltaSeconds)
 {
 	for (auto& uiEntity : updateUiEntities_)
 	{
-		uiEntity->Tick(deltaSeconds);
+		uiEntity.second->Tick(deltaSeconds);
 	}
 }
 
@@ -64,7 +40,7 @@ void GameTitleScene::Render()
 		{
 			for (auto& uiEntity : renderUiEntities_)
 			{
-				uiEntity->Render();
+				uiEntity.second->Render();
 			}
 		}
 		renderManager_->End();
@@ -78,4 +54,62 @@ void GameTitleScene::Enter()
 
 void GameTitleScene::Exit()
 {
+}
+
+void GameTitleScene::Initailize()
+{
+	uiCamera_ = entityManager_->GetByName<UICamera>("UICamera");
+	updateUiEntities_.insert({ "UICamera", uiCamera_ });
+
+	Backdrop* backdrop = entityManager_->Create<Backdrop>();
+	updateUiEntities_.insert({ "Backdrop", backdrop });
+	renderUiEntities_.insert({ "Backdrop", backdrop });
+
+	Title* title = entityManager_->Create<Title>();
+	updateUiEntities_.insert({ "Title", title });
+	renderUiEntities_.insert({ "Title", title });
+
+	UIButton* startBtn = entityManager_->Create<UIButton>("Resource\\UI\\Start.button", uiCamera_, glManager_->GetByName<TTFont>("Font48"), EMouse::LEFT, [&]() {});
+	updateUiEntities_.insert({ "StartButton", startBtn });
+	renderUiEntities_.insert({ "StartButton", startBtn });
+
+	UIButton* galleryBtn = entityManager_->Create<UIButton>("Resource\\UI\\Gallery.button", uiCamera_, glManager_->GetByName<TTFont>("Font48"), EMouse::LEFT, [&]() {});
+	updateUiEntities_.insert({ "GalleryButton", galleryBtn });
+	renderUiEntities_.insert({ "GalleryButton", galleryBtn });
+
+	UIButton* optionBtn = entityManager_->Create<UIButton>("Resource\\UI\\Option.button", uiCamera_, glManager_->GetByName<TTFont>("Font48"), EMouse::LEFT, [&]() {});
+	updateUiEntities_.insert({ "OptionButton", optionBtn });
+	renderUiEntities_.insert({ "OptionButton", optionBtn });
+
+	UIButton* quitBtn = entityManager_->Create<UIButton>("Resource\\UI\\Quit.button", uiCamera_, glManager_->GetByName<TTFont>("Font48"), EMouse::LEFT, [&]() {});
+	updateUiEntities_.insert({ "QuitButton", quitBtn });
+	renderUiEntities_.insert({ "QuitButton", quitBtn });
+}
+
+void GameTitleScene::UnInitailize()
+{
+	/** 외부에서 생성된 엔티티나 리소스는 초기화 해제하지 않습니다. */
+	for (auto& updateUiEntity : updateUiEntities_)
+	{
+		if (updateUiEntity.second == uiCamera_)
+		{
+			// UI 카메라는 외부에서 생성했으므로, 정리 대상에서 제외.
+			continue;
+		}
+
+		if (updateUiEntity.second && updateUiEntity.second->IsInitialized())
+		{
+			entityManager_->Destroy(updateUiEntity.second);
+			updateUiEntity.second = nullptr;
+		}
+	}
+	
+	for (auto& renderUIEntity : renderUiEntities_)
+	{
+		if (renderUIEntity.second && renderUIEntity.second->IsInitialized())
+		{
+			entityManager_->Destroy(renderUIEntity.second);
+			renderUIEntity.second = nullptr;
+		}
+	}
 }
