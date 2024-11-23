bef8a128db2764d5281f79bfb4f32ce3331bfa79
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2024-11-23 16:17:21 +0900

    UI 버튼을 클릭하면 특정 이벤트를 실행하는 기능 추가

--- Game/Inc/Entity/UIButton.h
+++ Game/Inc/Entity/UIButton.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <functional>
+#include <map>
 #include <string>
 
 #include "Entity/IEntity2D.h"
+#include "GLFW/GLFWManager.h"
 #include "Physic/Rect2D.h"
 
 /** 클래스의 전방 선언입니다. */
@@ -14,7 +16,7 @@ class UICamera;
 class UIButton : public IEntity2D
 {
 public:
-	UIButton(UICamera* uiCamera, TTFont* font);
+	UIButton(UICamera* uiCamera, TTFont* font, const EMouse& mouse, const std::function<void()>& clickEvent);
 	virtual ~UIButton();
 
 	DISALLOW_COPY_AND_ASSIGN(UIButton);
@@ -25,7 +27,7 @@ public:
 
 private:
 	/** UI 버튼의 상태입니다. */
-	enum class EButtonPress : int32_t
+	enum class EButtonState : int32_t
 	{
 		DISABLED = 0x00,
 		ENABLED  = 0x01,
@@ -33,13 +35,40 @@ private:
 		RELEASED = 0x03,
 	};
 
+	/** 입력 상태에 따른 버튼의 상태를 얻습니다. */
+	EButtonState GetButtonState(const EPress& press, const EButtonState& state);
+
+	/** 마우스 커서가 감지되는지 확인합니다. */
+	bool IsDetectMouseCursor();
+
 private:
 	/** UI 버튼의 입력 및 렌더링에 기준이 될 UI 카메라입니다. */
 	UICamera* uiCamera_ = nullptr;
 
 	/** UI 버튼에 텍스트를 렌더링할 때 참조할 폰트입니다. */
 	TTFont* font_ = nullptr;
+	
+	/** UI 버튼이 반응할 마우스 버튼의 종류입니다. */
+	EMouse mouse_ = EMouse::LEFT;
+
+	/** UI 버튼의 상태입니다. */
+	EButtonState state_ = EButtonState::DISABLED;
+
+	/** UI 버튼 클릭 시 수행할 이벤트입니다. */
+	std::function<void()> clickEvent_ = nullptr;
 
 	/** UI 버튼의 백그라운드 크기입니다. */
 	Rect2D background_;
+
+	/** UI 버튼에 표시할 텍스트입니다. */
+	std::wstring text_;
+	
+	/** UI 버튼에 표시할 텍스트의 색상입니다. */
+	glm::vec4 textColor_ = glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
+
+	/** UI 버튼에 표시할 텍스트의 외곽선입니다. */
+	glm::vec4 textOutlineColor_ = glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
+
+	/** UI 버튼의 상태 별 색상입니다. */
+	std::map<EButtonState, glm::vec4> stateColors_;
 };
--- Game/Src/Entity/UIButton.cpp
+++ Game/Src/Entity/UIButton.cpp
@@ -6,12 +6,26 @@
 #include "Utils/Assertion.h"
 #include "Utils/Utils.h"
 
-UIButton::UIButton(UICamera* uiCamera, TTFont* font)
+UIButton::UIButton(UICamera* uiCamera, TTFont* font, const EMouse& mouse, const std::function<void()>& clickEvent)
 	: uiCamera_(uiCamera)
 	, font_(font)
+	, mouse_(mouse)
+	, clickEvent_(clickEvent)
 {
-	tickOrder_ = 1;
-	renderOrder_ = 1;
+	tickOrder_ = 2;
+	renderOrder_ = 2;
+
+	background_ = Rect2D(glm::vec2(0.0f, 0.0f), glm::vec2(200.0f, 50.0f));
+	text_ = L"Hello";
+	textColor_ = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+	textOutlineColor_ = glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);
+	stateColors_ = 
+	{
+		{ EButtonState::DISABLED, glm::vec4(0.0f, 0.0f, 0.0f, 0.2f) },
+		{ EButtonState::ENABLED,  glm::vec4(0.0f, 0.0f, 0.0f, 0.6f) },
+		{ EButtonState::PRESSED,  glm::vec4(0.0f, 0.0f, 0.0f, 0.9f) },
+		{ EButtonState::RELEASED, glm::vec4(0.0f, 0.0f, 0.0f, 0.5f) },
+	};
 
 	bIsInitialized_ = true;
 }
@@ -26,11 +40,25 @@ UIButton::~UIButton()
 
 void UIButton::Tick(float deltaSeconds)
 {
+	EPress press = GLFWManager::GetRef().GetMousePress(mouse_);
+	state_ = GetButtonState(press, state_);
+
+	if (state_ == EButtonState::RELEASED && clickEvent_)
+	{
+		clickEvent_();
+	}
 }
 
 void UIButton::Render()
 {
-	renderManager_->DrawRectWireframe(background_.center, background_.size.x, background_.size.y, glm::vec4(1.0f, 0.0f, 0.0f, 0.0f), 0.0f);
+	renderManager_->DrawRectWireframe(
+		background_.center, 
+		background_.size.x, 
+		background_.size.y, 
+		stateColors_.at(state_),
+		0.0f
+	);
+
 }
 
 void UIButton::Release()
@@ -41,4 +69,75 @@ void UIButton::Release()
 	uiCamera_ = nullptr;
 
 	bIsInitialized_ = false;
+}
+
+UIButton::EButtonState UIButton::GetButtonState(const EPress& press, const EButtonState& state)
+{
+	if (!IsDetectMouseCursor())
+	{
+		if (state == EButtonState::PRESSED && press == EPress::HELD)
+		{
+			return EButtonState::PRESSED;
+		}
+		else
+		{
+			return EButtonState::DISABLED;
+		}
+	}
+
+	if (state == EButtonState::DISABLED)
+	{
+		if (press == EPress::HELD || press == EPress::PRESSED)
+		{
+			return EButtonState::DISABLED;
+		}
+		else // press == EPress::NONE || press == EPress::RELEASED
+		{
+			return EButtonState::ENABLED;
+		}
+	}
+	else if (state == EButtonState::ENABLED)
+	{
+		if (press == EPress::HELD || press == EPress::PRESSED)
+		{
+			return EButtonState::PRESSED;
+		}
+		else
+		{
+			return EButtonState::ENABLED;
+		}
+	}
+	else if (state == EButtonState::PRESSED)
+	{
+		if (press == EPress::RELEASED)
+		{
+			return EButtonState::RELEASED;
+		}
+		else if (press == EPress::HELD || press == EPress::PRESSED)
+		{
+			return EButtonState::PRESSED;
+		}
+		else // press == EPress::NONE
+		{
+			return EButtonState::ENABLED;
+		}
+	}
+	else // state == EButtonState::RELEASED
+	{
+		if (press == EPress::NONE || press == EPress::RELEASED)
+		{
+			return EButtonState::ENABLED;
+		}
+		else // press == EPress::HELD || press == EPress::PRESSED
+		{
+			return EButtonState::PRESSED;
+		}
+	}
+
+	return EButtonState::DISABLED;
+}
+
+bool UIButton::IsDetectMouseCursor()
+{
+	return background_.Intersect(uiCamera_->GetMousePointer());
 }
