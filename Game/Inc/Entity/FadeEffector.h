5acab850f0ac15c89d664b612637779788bb98fb
choijione <choijione02@gmail.com>
2024-11-25 08:20:01 +0900

    페이드 효과를 적용하는 기능 추가

--- Game/Inc/Entity/FadeEffector.h
+++ Game/Inc/Entity/FadeEffector.h
@@ -16,6 +16,50 @@ public:
 	virtual void Tick(float deltaSeconds) override;
 	virtual void Release() override;
 
-private:
+	/** 페이드 인 효과(화면이 어두었다가 서서히 밝아지는)를 시작합니다. */
+	void StartIn(float time);
 
+	/** 페이드 아웃 효과(화면이 밝았다가 서서이 어두어지는)를 시작합니다. */
+	void StartOut(float time);
+
+	/** 현재 페이드 효과가 시작되었는지 확인합니다. */
+	bool IsStart() const { return bIsStart_; }
+
+	/** 페이드 효과를 적용 할 색상을 얻습니다. */
+	const glm::vec3& GetBlendColor() const { return blendColor_; }
+
+	/** 페이드 효과를 적용할 색상의 가중치 값을 얻습니다.  */
+	float GetFactor() const { return factor_; }
+
+private:
+	/** 페이드 효과의 종류입니다. */
+	enum class EType
+	{
+		NONE     = 0x00,
+		FADE_IN  = 0x01,
+		FADE_OUT = 0x02,
+	};
+
+private:
+	/** 페이드 효과 적용 시 프레임 버퍼에 블랜딩할 색상입니다. 이 색상은 검정색 고정입니다. */
+	glm::vec3 blendColor_ = glm::vec3(0.0f, 0.0f, 0.0f);
+
+	/**
+	 * 페이드 효과 적용 시 색상의 가중치 값입니다. 0.0이면 색상을 혼합하지 않고, 1.0이면 혼합 색상만 표시합니다.
+	 * 0.0 => Fade In
+	 * 1.0 => Fade Out
+	 */
+	float factor_ = 0.0f;
+
+	/** 페이드 효과 적용 시간 값입니다. */
+	float effectTime_ = 0.0f;
+
+	/** 페이드 효과 적용 전체 시간 값입니다. */
+	float maxEffectTime_ = 0.0f;
+
+	/** 현재 페이드 효과입니다. */
+	EType type_ = EType::NONE;
+
+	/** 페이드 효과가 시작되었는지 확인합니다. */
+	bool bIsStart_ = false;
 };
--- Game/Src/Entity/FadeEffector.cpp
+++ Game/Src/Entity/FadeEffector.cpp
@@ -19,6 +19,21 @@ FadeEffector::~FadeEffector()
 
 void FadeEffector::Tick(float deltaSeconds)
 {
+	if (!bIsStart_)
+	{
+		return;
+	}
+
+	float bias = (type_ == EType::FADE_IN) ? -1.0f : 1.0f;
+	effectTime_ += deltaSeconds * bias;
+	effectTime_ = glm::clamp<float>(effectTime_, 0.0f, maxEffectTime_);
+
+	factor_ = effectTime_ / maxEffectTime_;
+	if (factor_ <= 0.0f || factor_ >= 1.0f)
+	{
+		type_ = EType::NONE;
+		bIsStart_ = false;
+	}
 }
 
 void FadeEffector::Release()
@@ -26,4 +41,22 @@ void FadeEffector::Release()
 	CHECK(bIsInitialized_);
 
 	bIsInitialized_ = false;
+}
+
+// factor 값이 1.0 => 0.0
+void FadeEffector::StartIn(float time)
+{
+	bIsStart_ = true;
+	type_ = EType::FADE_IN;
+	effectTime_ = time;
+	maxEffectTime_ = time;
+}
+
+// factor 값이 0.0 => 1.0
+void FadeEffector::StartOut(float time)
+{
+	bIsStart_ = true;
+	type_ = EType::FADE_OUT;
+	effectTime_ = 0.0f;
+	maxEffectTime_ = time;
 }
