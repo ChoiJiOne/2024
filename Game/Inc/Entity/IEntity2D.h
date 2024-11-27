e46dd8be13868c9bc4d301883d5de56b230ca834
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2024-11-27 15:12:53 +0900

    2D 엔티티 인터페이스의 렌더링 순위 값을 나타내는 멤버 변수 삭제

--- Game/Inc/Entity/IEntity2D.h
+++ Game/Inc/Entity/IEntity2D.h
@@ -15,16 +15,7 @@ public:
 	virtual void Render() = 0;
 	virtual void Release() = 0;
 
-	/** 2D 엔티티의 렌더링 순서 값을 설정합니다. */
-	void SetRenderOrder(uint32_t renderOrder) { renderOrder_ = renderOrder; }
-
-	/** 2D 엔티티의 렌더링 순서 값을 얻습니다. */
-	uint32_t GetRenderOrder() const { return renderOrder_; }
-
 protected:
-	/** 2D 엔티티의 렌더링 순위 값입니다. 이 값은 낮을수록 우선순위가 높아집니다. */
-	uint32_t renderOrder_ = 0;
-
 	/** 렌더링할 때 사용할 2D 렌더러입니다. */
 	class RenderManager2D* renderManager_ = nullptr;;
 };
