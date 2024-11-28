ce01a212be9118d0a4eb5f4325e01153e4b14e37
choijione <choijione02@gmail.com>
2024-11-28 09:57:26 +0900

    게임 씬 인터페이스의 멤버 변수에 게임 매니저의 포인터 추가

--- Game/Src/Scene/IGameScene.cpp
+++ Game/Src/Scene/IGameScene.cpp
@@ -1,6 +1,7 @@
 #include "Audio/AudioManager.h"
 #include "Crash/CrashManager.h"
 #include "Entity/EntityManager.h"
+#include "Game/GameManager.h"
 #include "GL/GLManager.h"
 #include "GL/RenderManager2D.h"
 #include "GLFW/GLFWManager.h"
@@ -11,6 +12,7 @@ IGameScene::IGameScene()
 	audioManager_ = AudioManager::GetPtr();
 	crashManager_ = CrashManager::GetPtr();
 	entityManager_ = EntityManager::GetPtr();
+	gameManager_ = GameManager::GetPtr();
 	glManager_ = GLManager::GetPtr();
 	renderManager_ = RenderManager2D::GetPtr();
 	glfwManager_ = GLFWManager::GetPtr();
@@ -23,6 +25,7 @@ IGameScene::~IGameScene()
 	glfwManager_ = nullptr;
 	renderManager_ = nullptr;
 	glManager_ = nullptr;
+	gameManager_ = nullptr;
 	entityManager_ = nullptr;
 	crashManager_ = nullptr;
 	audioManager_ = nullptr;
