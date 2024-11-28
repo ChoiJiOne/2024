6a64da179b3eb636f0d989cced6c77c2c27b9fad
choijione <choijione02@gmail.com>
2024-11-28 09:47:15 +0900

    게임 플레이 기록을 나타내는 구조체 추가

--- Game/Inc/Game/GameManager.h
+++ Game/Inc/Game/GameManager.h
@@ -4,6 +4,28 @@
 
 #include "Utils/Macro.h"
 
+/** 게임 플레이 기록입니다. */
+struct GamePlayRecord
+{
+	float   playTime           = 0.0f; // 플레이 타임입니다.
+	float   healHp             = 0.0f; // 회복한 체력입니다.
+	float   lostHp             = 0.0f; // 잃은 체력입니다.
+	float   healMp             = 0.0f; // 회복한 마나입니다.
+	float   lostMp             = 0.0f; // 잃은 마나입니다.
+	int32_t getCoin            = 0; // 획득한 코인 수입니다.
+	int32_t useSkill           = 0; // 사용한 스킬 수입니다.
+	int32_t genFire            = 0; // 생성된 불 엔티티의 수입니다.
+	int32_t genCoin            = 0; // 생성된 코인의 수입니다.
+	int32_t genRedPotion       = 0; // 생성된 빨간(HP 회복) 포션 엔티티의 수입니다.
+	int32_t genBluePotion      = 0; // 생성된 파란(MP 회복) 포션 엔티티의 수입니다.
+	int32_t genPowerRedPotion  = 0; // 생성된 빨간(HP 전체 회복) 파워 포션 엔티티의 수입니다.
+	int32_t genPowerBluePotion = 0; // 생성된 파란(MP 전체 회복) 파워 포션 엔티티의 수입니다.
+
+	static const uint32_t MAX_TIME_LENGTH = 20;
+	char startPlayTime[MAX_TIME_LENGTH]; // 게임 시작 시간입니다.
+	char endPlayTime[MAX_TIME_LENGTH];   // 게임 종료 시간입니다.
+};
+
 /** 
  * 전체적인 게임의 속성을 관리하는 엔티티입니다. 
  * 이때, 이 매니저 클래스는 싱글턴입니다.
@@ -43,4 +65,7 @@ private:
 
 	/** 이름에 대응하는 설정 값입니다. */
 	std::map<std::string, float> configValues_;
+
+	/** 게임 플레이 기록입니다. */
+	std::vector<GamePlayRecord> gamePlayRecords_;
 };
